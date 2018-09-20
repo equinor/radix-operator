1098d93b780a58d0ef68bd7d7582ca3af0e6bffd
Kjell Erik Reed Anda <keaaa@statoil.com>
2018-09-20 15:15:31 +0200

    use wildcard and kubec to manage tls certs

---
+++ pkg/apis/kube/namespaces.go
@@ -0,0 +1,28 @@
+package kube
+
+import (
+	log "github.com/Sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (kube *Kube) ApplyNamespace(name string, labels map[string]string, ownerRefs []metav1.OwnerReference) error {
+	log.Infof("Create namespace: %s", name)
+
+	namespace := corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            name,
+			OwnerReferences: ownerRefs,
+			Labels:          labels,
+		},
+	}
+	_, err := kube.kubeClient.CoreV1().Namespaces().Create(&namespace)
+
+	if errors.IsAlreadyExists(err) {
+		log.Infof("Namespace already exist %s", name)
+		return nil
+	}
+
+	return err
+}
