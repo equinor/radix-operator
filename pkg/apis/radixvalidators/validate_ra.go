d70b33935d8235e1efc26fe7604bb6e65758f8c3
Kjell Erik Reed Anda <keaaa@statoil.com>
2018-11-30 15:26:21 +0100

    OR-510: ra validation on pipeline startup

--- pkg/apis/radixvalidators/validate_ra.go
+++ pkg/apis/radixvalidators/validate_ra.go
@@ -2,20 +2,39 @@ package radixvalidators
 
 import (
 	"fmt"
+	"regexp"
 
 	radixv1 "github.com/statoil/radix-operator/pkg/apis/radix/v1"
 	radixclient "github.com/statoil/radix-operator/pkg/client/clientset/versioned"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func CanRadixApplicationBeInserted(client radixclient.Interface, app *radixv1.RadixApplication) (bool, error) {
+	isValid, errs := CanRadixApplicationBeInsertedErrors(client, app)
+	if isValid {
+		return true, nil
+	}
+
+	return false, ConcatErrors(errs)
+}
+
+func CanRadixApplicationBeInsertedErrors(client radixclient.Interface, app *radixv1.RadixApplication) (bool, []error) {
 	errs := []error{}
 	err := validateAppName(app.Name)
 	if err != nil {
 		errs = append(errs, err)
 	}
 
-	err = validateExistEnvForComponentVariables(app)
+	componentErrs := validateComponents(app)
+	if len(componentErrs) > 0 {
+		errs = append(errs, componentErrs...)
+	}
+
+	err = validateEnvNames(app)
+	if err != nil {
+		errs = append(errs, err)
+	}
+
+	err = validateBranchNames(app)
 	if err != nil {
 		errs = append(errs, err)
 	}
@@ -28,30 +47,76 @@ func CanRadixApplicationBeInserted(client radixclient.Interface, app *radixv1.Ra
 	if len(errs) <= 0 {
 		return true, nil
 	}
-	return false, concatErrors(errs)
+	return false, errs
 }
 
-func validateDoesRRExist(client radixclient.Interface, appName string) error {
-	rr, err := client.RadixV1().RadixRegistrations("default").Get(appName, metav1.GetOptions{})
-	if rr == nil {
-		return fmt.Errorf("No app registered with that name %s", appName)
-	}
-	if err != nil {
-		return fmt.Errorf("Could not get app registration obj %s", appName)
-	}
-	return nil
-}
-
-func validateExistEnvForComponentVariables(app *radixv1.RadixApplication) error {
+func validateComponents(app *radixv1.RadixApplication) []error {
+	errs := []error{}
 	for _, component := range app.Spec.Components {
+		err := validateRequiredResourceName("component name", component.Name)
+		if err != nil {
+			errs = append(errs, err)
+		}
+
+		err = validateReplica(component.Replicas)
+		if err != nil {
+			errs = append(errs, err)
+		}
+
+		for _, port := range component.Ports {
+			err := validateRequiredResourceName("port name", port.Name)
+			if err != nil {
+				errs = append(errs, err)
+			}
+		}
+
 		for _, variable := range component.EnvironmentVariables {
 			if !doesEnvExist(app, variable.Environment) {
-				return fmt.Errorf("Env %s refered to by component variable %s is not defined", variable.Environment, component.Name)
+				err = fmt.Errorf("Env %s refered to by component variable %s is not defined", variable.Environment, component.Name)
+				errs = append(errs, err)
 			}
 		}
 	}
 
+	return errs
+}
+
+func validateBranchNames(app *radixv1.RadixApplication) error {
+	for _, env := range app.Spec.Environments {
+		if env.Build.From == "" {
+			continue
+		}
+
+		err := validateLabelName("branch from", env.Build.From)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func validateEnvNames(app *radixv1.RadixApplication) error {
+	for _, env := range app.Spec.Environments {
+		err := validateRequiredResourceName("env name", env.Name)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func validateLabelName(resourceName, value string) error {
+	if len(value) > 253 {
+		return fmt.Errorf("%s (%s) max length is 253", resourceName, value)
+	}
+
+	re := regexp.MustCompile("^(([A-Za-z0-9][-A-Za-z0-9.]*)?[A-Za-z0-9])?$")
+
+	isValid := re.MatchString(value)
+	if isValid {
+		return nil
+	}
+	return fmt.Errorf("%s %s can only consist of alphanumeric characters, '.' and '-'", resourceName, value)
 }
 
 func doesEnvExist(app *radixv1.RadixApplication, name string) bool {
