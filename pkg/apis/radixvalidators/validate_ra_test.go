4b8a847fccf6dc4546299eed883a64be7f2fc95a
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-02-26 07:03:26 +0100

    Changed validation to match Kubernetes format

--- pkg/apis/radixvalidators/validate_ra_test.go
+++ pkg/apis/radixvalidators/validate_ra_test.go
@@ -88,6 +88,86 @@ func Test_invalid_ra(t *testing.T) {
 	}
 }
 
+func Test_ValidRALimitRequest_NoError(t *testing.T) {
+	var testScenarios = []struct {
+		name     string
+		updateRA updateRAFunc
+	}{
+		{"resource memory correct format: 50", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50"
+		}},
+		{"resource limit correct format: 50T", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50T"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50T"
+		}},
+		{"resource limit correct format: 50G", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50G"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50G"
+		}},
+		{"resource limit correct format: 50M", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50M"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50M"
+		}},
+		{"resource limit correct format: 50k", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50k"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50k"
+		}},
+		{"resource limit correct format: 50Gi", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50Gi"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50Gi"
+		}},
+		{"resource limit correct format: 50Mi", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50Mi"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50Mi"
+		}},
+		{"resource limit correct format: 50Ki", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50Ki"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50Ki"
+		}},
+	}
+
+	_, client := validRASetup()
+	for _, testcase := range testScenarios {
+		t.Run(testcase.name, func(t *testing.T) {
+			validRA := createValidRA()
+			testcase.updateRA(validRA)
+			isValid, err := radixvalidators.CanRadixApplicationBeInserted(client, validRA)
+
+			assert.True(t, isValid)
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func Test_InvalidRALimitRequest_Error(t *testing.T) {
+	var testScenarios = []struct {
+		name     string
+		updateRA updateRAFunc
+	}{
+		{"resource limit incorrect format: 50MB", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50MB"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50MB"
+		}},
+		{"resource limit incorrect format: 50K", func(ra *v1.RadixApplication) {
+			ra.Spec.Components[0].Resources.Limits["memory"] = "50K"
+			ra.Spec.Components[0].Resources.Requests["memory"] = "50K"
+		}},
+	}
+
+	_, client := validRASetup()
+	for _, testcase := range testScenarios {
+		t.Run(testcase.name, func(t *testing.T) {
+			validRA := createValidRA()
+			testcase.updateRA(validRA)
+			isValid, err := radixvalidators.CanRadixApplicationBeInserted(client, validRA)
+
+			assert.False(t, isValid)
+			assert.NotNil(t, err)
+		})
+	}
+}
+
 func createValidRA() *v1.RadixApplication {
 	validRA, _ := utils.GetRadixApplication("testdata/radixconfig.yaml")
 
