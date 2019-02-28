54434f2e4689662c843423f479b46628e3372cc1
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-02-28 16:56:30 +0100

    Made all tests run green after merge

--- pkg/apis/radixvalidators/validate_ra_test.go
+++ pkg/apis/radixvalidators/validate_ra_test.go
@@ -99,36 +99,36 @@ func Test_ValidRALimitRequest_NoError(t *testing.T) {
 		updateRA updateRAFunc
 	}{
 		{"resource memory correct format: 50", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50"
 		}},
 		{"resource limit correct format: 50T", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50T"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50T"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50T"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50T"
 		}},
 		{"resource limit correct format: 50G", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50G"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50G"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50G"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50G"
 		}},
 		{"resource limit correct format: 50M", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50M"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50M"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50M"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50M"
 		}},
 		{"resource limit correct format: 50k", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50k"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50k"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50k"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50k"
 		}},
 		{"resource limit correct format: 50Gi", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50Gi"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50Gi"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50Gi"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50Gi"
 		}},
 		{"resource limit correct format: 50Mi", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50Mi"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50Mi"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50Mi"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50Mi"
 		}},
 		{"resource limit correct format: 50Ki", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50Ki"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50Ki"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50Ki"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50Ki"
 		}},
 	}
 
@@ -151,12 +151,12 @@ func Test_InvalidRALimitRequest_Error(t *testing.T) {
 		updateRA updateRAFunc
 	}{
 		{"resource limit incorrect format: 50MB", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50MB"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50MB"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50MB"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50MB"
 		}},
 		{"resource limit incorrect format: 50K", func(ra *v1.RadixApplication) {
-			ra.Spec.Components[0].Resources.Limits["memory"] = "50K"
-			ra.Spec.Components[0].Resources.Requests["memory"] = "50K"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Limits["memory"] = "50K"
+			ra.Spec.Components[0].EnvironmentConfig[0].Resources.Requests["memory"] = "50K"
 		}},
 	}
 
