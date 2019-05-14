73d265114bd8455f2d3aaf1876f88c358b45e1bd
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-05-14 13:33:06 +0200

    Refactored into constant

--- pkg/apis/utils/github/clone.go
+++ pkg/apis/utils/github/clone.go
@@ -2,14 +2,16 @@ package github
 
 import corev1 "k8s.io/api/core/v1"
 
+// The script to ensure that github reponds before cloning. It breaks after max attempts
+const waitForGithubToRespond = "n=1;max=10;delay=5;while true; do if [ '$n' -lt '$max' ]; then nslookup github.com && break; n=$((n+1)); sleep $(($delay*$n)); else echo 'The command has failed after $n attempts.'; break; fi done"
+
 // CloneInitContainers The sidecars for cloning repo
 func CloneInitContainers(sshURL, branch, buildContextVolumeName, workspace, gitSSHKeyVolumeName string) []corev1.Container {
 	containers := []corev1.Container{
 		{
-			Name:  "nslookup",
-			Image: "alpine",
-			Args: []string{
-				"n=1;max=10;delay=5;while true; do if [ '$n' -lt '$max' ]; then nslookup github.com && break; n=$((n+1)); sleep $(($delay*$n)); else echo 'The command has failed after $n attempts.'; break; fi done"},
+			Name:            "nslookup",
+			Image:           "alpine",
+			Args:            []string{waitForGithubToRespond},
 			ImagePullPolicy: "Always",
 		},
 		{
