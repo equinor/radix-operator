10e00f719f6bf5781fa8fbd056c98d1ecae432d1
ingeknudsen <iknu@equinor.com>
2019-05-15 16:31:24 +0200

    Ra 295 use official git image from alpine (#238)
    * Remove use of gitclone image and introduce
    nslookup to add robustness
    
    * Refactored into constant
    
    * Refactored into git package +
    made constants public
    
    * Added command to container
    
    * Script had error
    
    * Need to have exponential growth, but turn down
    delay

---
+++ pkg/apis/utils/git/clone.go
@@ -0,0 +1,55 @@
+package git
+
+import corev1 "k8s.io/api/core/v1"
+
+const (
+	// GitSSHKeyVolumeName Deploy key + known_hosts
+	GitSSHKeyVolumeName = "git-ssh-keys"
+
+	// BuildContextVolumeName Name of volume to hold build context
+	BuildContextVolumeName = "build-context"
+
+	// Workspace Folder to hold the code to build
+	Workspace = "/workspace"
+
+	// The script to ensure that github reponds before cloning. It breaks after max attempts
+	waitForGithubToRespond = "n=1;max=10;delay=2;while true; do if [ \"$n\" -lt \"$max\" ]; then nslookup github.com && break; n=$((n+1)); sleep $(($delay*$n)); else echo \"The command has failed after $n attempts.\"; break; fi done"
+)
+
+// CloneInitContainers The sidecars for cloning repo
+func CloneInitContainers(sshURL, branch string) []corev1.Container {
+	containers := []corev1.Container{
+		{
+			Name:            "nslookup",
+			Image:           "alpine",
+			Args:            []string{waitForGithubToRespond},
+			Command:         []string{"/bin/sh", "-c"},
+			ImagePullPolicy: "Always",
+		},
+		{
+			Name:  "clone",
+			Image: "alpine/git",
+			Args: []string{
+				"clone",
+				sshURL,
+				"-b",
+				branch,
+				"--verbose",
+				"--progress",
+				"/workspace"},
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      BuildContextVolumeName,
+					MountPath: Workspace,
+				},
+				{
+					Name:      GitSSHKeyVolumeName,
+					MountPath: "/root/.ssh",
+					ReadOnly:  true,
+				},
+			},
+		},
+	}
+
+	return containers
+}
