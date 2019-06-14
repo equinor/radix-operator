e320efd7fad491708705b8261be9e50455c463e6
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-06-14 14:12:10 +0200

    Added to test to pick up another bug

--- pkg/apis/deployment/deployment_test.go
+++ pkg/apis/deployment/deployment_test.go
@@ -887,7 +887,8 @@ func TestObjectUpdated_WithOneExternalAliasRemovedOrModified_AllChangesPropelyRe
 				WithPort("http", 8080).
 				WithPublicPort("http").
 				WithDNSExternalAlias("some.alias.com").
-				WithDNSExternalAlias("another.alias.com")))
+				WithDNSExternalAlias("another.alias.com").
+				WithSecrets([]string{"a_secret"})))
 
 	// Test
 	ingresses, _ := client.ExtensionsV1beta1().Ingresses(envNamespace).List(metav1.ListOptions{})
@@ -900,9 +901,9 @@ func TestObjectUpdated_WithOneExternalAliasRemovedOrModified_AllChangesPropelyRe
 	assert.Equal(t, int32(8080), ingresses.Items[1].Spec.Rules[0].HTTP.Paths[0].Backend.ServicePort.IntVal, "Correct service port")
 
 	roles, _ := client.RbacV1().Roles(envNamespace).List(metav1.ListOptions{})
-	assert.Equal(t, 2, len(roles.Items[0].Rules[0].ResourceNames))
-	assert.Equal(t, "some.alias.com", roles.Items[0].Rules[0].ResourceNames[0], "Expected role should be able to access TLS certificate for external alias")
-	assert.Equal(t, "another.alias.com", roles.Items[0].Rules[0].ResourceNames[1], "Expected role should be able to access TLS certificate for second external alias")
+	assert.Equal(t, 3, len(roles.Items[0].Rules[0].ResourceNames))
+	assert.Equal(t, "some.alias.com", roles.Items[0].Rules[0].ResourceNames[1], "Expected role should be able to access TLS certificate for external alias")
+	assert.Equal(t, "another.alias.com", roles.Items[0].Rules[0].ResourceNames[2], "Expected role should be able to access TLS certificate for second external alias")
 
 	applyDeploymentWithSync(tu, client, radixclient, utils.ARadixDeployment().
 		WithAppName(anyAppName).
@@ -913,7 +914,8 @@ func TestObjectUpdated_WithOneExternalAliasRemovedOrModified_AllChangesPropelyRe
 				WithPort("http", 8081).
 				WithPublicPort("http").
 				WithDNSExternalAlias("some.alias.com").
-				WithDNSExternalAlias("yet.another.alias.com")))
+				WithDNSExternalAlias("yet.another.alias.com").
+				WithSecrets([]string{"a_secret"})))
 
 	ingresses, _ = client.ExtensionsV1beta1().Ingresses(envNamespace).List(metav1.ListOptions{})
 	assert.Equal(t, 3, len(ingresses.Items), "Environment should have three ingresses")
@@ -927,9 +929,9 @@ func TestObjectUpdated_WithOneExternalAliasRemovedOrModified_AllChangesPropelyRe
 	assert.Equal(t, int32(8081), ingresses.Items[2].Spec.Rules[0].HTTP.Paths[0].Backend.ServicePort.IntVal, "Correct service port")
 
 	roles, _ = client.RbacV1().Roles(envNamespace).List(metav1.ListOptions{})
-	assert.Equal(t, 2, len(roles.Items[0].Rules[0].ResourceNames))
-	assert.Equal(t, "some.alias.com", roles.Items[0].Rules[0].ResourceNames[0], "Expected role should be able to access TLS certificate for external alias")
-	assert.Equal(t, "yet.another.alias.com", roles.Items[0].Rules[0].ResourceNames[1], "Expected role should be able to access TLS certificate for second external alias")
+	assert.Equal(t, 3, len(roles.Items[0].Rules[0].ResourceNames))
+	assert.Equal(t, "some.alias.com", roles.Items[0].Rules[0].ResourceNames[1], "Expected role should be able to access TLS certificate for external alias")
+	assert.Equal(t, "yet.another.alias.com", roles.Items[0].Rules[0].ResourceNames[2], "Expected role should be able to access TLS certificate for second external alias")
 
 	applyDeploymentWithSync(tu, client, radixclient, utils.ARadixDeployment().
 		WithAppName(anyAppName).
@@ -939,7 +941,8 @@ func TestObjectUpdated_WithOneExternalAliasRemovedOrModified_AllChangesPropelyRe
 				WithName(anyComponentName).
 				WithPort("http", 8081).
 				WithPublicPort("http").
-				WithDNSExternalAlias("yet.another.alias.com")))
+				WithDNSExternalAlias("yet.another.alias.com").
+				WithSecrets([]string{"a_secret"})))
 
 	ingresses, _ = client.ExtensionsV1beta1().Ingresses(envNamespace).List(metav1.ListOptions{})
 	assert.Equal(t, 2, len(ingresses.Items), "Environment should have two ingresses")
@@ -948,8 +951,8 @@ func TestObjectUpdated_WithOneExternalAliasRemovedOrModified_AllChangesPropelyRe
 	assert.Equal(t, int32(8081), ingresses.Items[1].Spec.Rules[0].HTTP.Paths[0].Backend.ServicePort.IntVal, "Correct service port")
 
 	roles, _ = client.RbacV1().Roles(envNamespace).List(metav1.ListOptions{})
-	assert.Equal(t, 1, len(roles.Items[0].Rules[0].ResourceNames))
-	assert.Equal(t, "yet.another.alias.com", roles.Items[0].Rules[0].ResourceNames[0], "Expected role should be able to access TLS certificate for second external alias")
+	assert.Equal(t, 2, len(roles.Items[0].Rules[0].ResourceNames))
+	assert.Equal(t, "yet.another.alias.com", roles.Items[0].Rules[0].ResourceNames[1], "Expected role should be able to access TLS certificate for second external alias")
 
 	// Remove app alias from dev
 	applyDeploymentWithSync(tu, client, radixclient, utils.ARadixDeployment().
