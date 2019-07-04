693a3995e5470d75c3d8884ecc2d0084f14339c1
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-07-04 08:55:49 +0200

    Finally got a working prototype

--- pipeline-runner/builderx/buildx_acr.sh
+++ pipeline-runner/builderx/buildx_acr.sh
@@ -10,11 +10,17 @@ fi
 echo "Login to azure..."
 az login --service-principal -u ${SP_USER} -p ${SP_SECRET} --tenant ${TENANT}
 
-echo "Build with buildx (5)..."
+echo "Copy file to workspace folder"
+cp /radix-image-builderx/build_with_cache.yaml ${CONTEXT}/build_with_cache.yaml
+
+cat ${CONTEXT}/build_with_cache.yaml
+
+echo "Build with buildx (13)..."
 az acr run --registry ${DOCKER_REGISTRY} -f build_with_cache.yaml \
     --set REGISTRY=${DOCKER_REGISTRY}.azurecr.io \
     --set IMAGE=${IMAGE} \
     --set PUSH=${PUSH} \
     --set BUILD_CONTEXT=${CONTEXT} \
+    --set DOCKER_FILE_NAME=${DOCKER_FILE_NAME} \
     --set REPOSITORY_NAME=${REPOSITORY_NAME} \
-    ./${DOCKER_FILE_NAME}
+    ${CONTEXT}/
