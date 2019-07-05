a6e054251cb99e5fb9b84dafaef54b1ac43dab67
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-07-05 13:45:53 +0200

    Patch namespace if exists

--- pkg/apis/kube/namespaces.go
+++ pkg/apis/kube/namespaces.go
@@ -1,10 +1,15 @@
 package kube
 
 import (
+	"encoding/json"
+	"fmt"
+
 	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
 )
 
 func (kube *Kube) ApplyNamespace(name string, labels map[string]string, ownerRefs []metav1.OwnerReference) error {
@@ -20,7 +25,37 @@ func (kube *Kube) ApplyNamespace(name string, labels map[string]string, ownerRef
 	_, err := kube.kubeClient.CoreV1().Namespaces().Create(&namespace)
 
 	if errors.IsAlreadyExists(err) {
-		log.Debugf("Namespace already exist %s", name)
+		log.Debugf("Namespace object %s already exists, updating the object now", name)
+		oldNamespace, err := kube.kubeClient.CoreV1().Namespaces().Get(name, metav1.GetOptions{})
+		if err != nil {
+			return fmt.Errorf("Failed to get old Ingress object: %v", err)
+		}
+
+		newNamespace := oldNamespace.DeepCopy()
+		newNamespace.ObjectMeta.OwnerReferences = ownerRefs
+		newNamespace.ObjectMeta.Labels = labels
+
+		oldNamespaceJSON, err := json.Marshal(oldNamespace)
+		if err != nil {
+			return fmt.Errorf("Failed to marshal old namespace object: %v", err)
+		}
+
+		newNamespaceJSON, err := json.Marshal(newNamespace)
+		if err != nil {
+			return fmt.Errorf("Failed to marshal new namespace object: %v", err)
+		}
+
+		patchBytes, err := strategicpatch.CreateTwoWayMergePatch(oldNamespaceJSON, newNamespaceJSON, corev1.Namespace{})
+		if err != nil {
+			return fmt.Errorf("Failed to create two way merge patch namespace objects: %v", err)
+		}
+
+		patchedNamespace, err := kube.kubeClient.CoreV1().Namespaces().Patch(name, types.StrategicMergePatchType, patchBytes)
+		if err != nil {
+			return fmt.Errorf("Failed to patch namespace object: %v", err)
+		}
+
+		log.Debugf("Patched namespace: %s ", patchedNamespace.Name)
 		return nil
 	}
 
