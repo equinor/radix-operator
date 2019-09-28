71da062b5fad8d94d2ebb9ed6e4f7bfbfb6b33af
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-09-28 15:29:52 +0200

    Tried to optimize service

--- pkg/apis/deployment/service.go
+++ pkg/apis/deployment/service.go
@@ -20,46 +20,52 @@ import (
 func (deploy *Deployment) createService(deployComponent v1.RadixDeployComponent) error {
 	namespace := deploy.radixDeployment.Namespace
 	service := getServiceConfig(deployComponent.Name, deploy.radixDeployment, deployComponent.Ports)
-	log.Debugf("Creating Service object %s in namespace %s", deployComponent.Name, namespace)
-	createdService, err := deploy.kubeclient.CoreV1().Services(namespace).Create(service)
-	if errors.IsAlreadyExists(err) {
-		log.Debugf("Service object %s already exists in namespace %s, updating the object now", deployComponent.Name, namespace)
-		oldService, err := deploy.kubeclient.CoreV1().Services(namespace).Get(deployComponent.Name, metav1.GetOptions{})
-		if err != nil {
-			return fmt.Errorf("Failed to get old Service object: %v", err)
-		}
-		newService := oldService.DeepCopy()
-		ports := buildServicePorts(deployComponent.Ports)
 
-		newService.Spec.Ports = ports
-		newService.ObjectMeta.OwnerReferences = service.ObjectMeta.OwnerReferences
+	oldService, err := deploy.getService(deployComponent.Name)
+	if err != nil && errors.IsNotFound(err) {
+		log.Debugf("Creating Service object %s in namespace %s", deployComponent.Name, namespace)
+		createdService, err := deploy.kubeclient.CoreV1().Services(namespace).Create(service)
 
-		oldServiceJSON, err := json.Marshal(oldService)
 		if err != nil {
-			return fmt.Errorf("Failed to marshal old Service object: %v", err)
+			return fmt.Errorf("Failed to create Service object: %v", err)
 		}
 
-		newServiceJSON, err := json.Marshal(newService)
-		if err != nil {
-			return fmt.Errorf("Failed to marshal new Service object: %v", err)
-		}
+		log.Debugf("Created Service: %s in namespace %s", createdService.Name, namespace)
+		return nil
+	}
 
-		patchBytes, err := strategicpatch.CreateTwoWayMergePatch(oldServiceJSON, newServiceJSON, corev1.Service{})
-		if err != nil {
-			return fmt.Errorf("Failed to create two way merge patch Service objects: %v", err)
-		}
+	log.Debugf("Service object %s already exists in namespace %s, updating the object now", deployComponent.Name, namespace)
+	newService := oldService.DeepCopy()
+	ports := buildServicePorts(deployComponent.Ports)
 
+	newService.Spec.Ports = ports
+	newService.ObjectMeta.OwnerReferences = service.ObjectMeta.OwnerReferences
+
+	oldServiceJSON, err := json.Marshal(oldService)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal old Service object: %v", err)
+	}
+
+	newServiceJSON, err := json.Marshal(newService)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal new Service object: %v", err)
+	}
+
+	patchBytes, err := strategicpatch.CreateTwoWayMergePatch(oldServiceJSON, newServiceJSON, corev1.Service{})
+	if err != nil {
+		return fmt.Errorf("Failed to create two way merge patch Service objects: %v", err)
+	}
+
+	if !isEmptyPatch(patchBytes) {
 		patchedService, err := deploy.kubeclient.CoreV1().Services(namespace).Patch(deployComponent.Name, types.StrategicMergePatchType, patchBytes)
 		if err != nil {
 			return fmt.Errorf("Failed to patch Service object: %v", err)
 		}
-		log.Debugf("Patched Service: %s in namespace %s", patchedService.Name, namespace)
-		return nil
+		log.Debugf("#########YALLA##########Patched Service: %s in namespace %s", patchedService.Name, namespace)
+	} else {
+		log.Debugf("#########YALLA##########No need to patch service: %s ", deployComponent.Name)
 	}
-	if err != nil {
-		return fmt.Errorf("Failed to create Service object: %v", err)
-	}
-	log.Debugf("Created Service: %s in namespace %s", createdService.Name, namespace)
+
 	return nil
 }
 
@@ -112,6 +118,25 @@ func (deploy *Deployment) listServices() ([]*corev1.Service, error) {
 	return services, nil
 }
 
+func (deploy *Deployment) getService(name string) (*corev1.Service, error) {
+	var service *corev1.Service
+	var err error
+
+	if deploy.serviceLister != nil {
+		service, err = deploy.serviceLister.Services(deploy.radixDeployment.GetNamespace()).Get(name)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		service, err = deploy.kubeclient.CoreV1().Services(deploy.radixDeployment.GetNamespace()).Get(name, metav1.GetOptions{})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return service, nil
+}
+
 func getServiceConfig(componentName string, radixDeployment *v1.RadixDeployment, componentPorts []v1.ComponentPort) *corev1.Service {
 	ownerReference := getOwnerReferenceOfDeployment(radixDeployment)
 
@@ -149,4 +174,8 @@ func buildServicePorts(componentPorts []v1.ComponentPort) []corev1.ServicePort {
 		ports = append(ports, servicePort)
 	}
 	return ports
+}
+
+func isEmptyPatch(patchBytes []byte) bool {
+	return string(patchBytes) == "{}"
 }
