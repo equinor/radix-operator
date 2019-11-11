eac2d20e8dbd8ca8ae42978086560c966647b2fd
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-11-11 13:42:41 +0100

    Echo environment variables

--- pipeline-runner/builder/build_acr.sh
+++ pipeline-runner/builder/build_acr.sh
@@ -11,20 +11,21 @@ function GetBuildCommand() {
   local secretName
   local secretValue
 
+  $(echo env)
+
   while read -r line; do
       if [[ "$line" ]]; then
           keyValue=(${line//=/ })
           envBuildSecret=${keyValue[0]}
           secretName=${envBuildSecret#"$prefix"}
           secretValue="$(printenv $envBuildSecret | base64)"
-          buildArgs+="--secret-build-arg $secretName=$secretValue "
+          buildArgs+="--secret-build-arg $secretName=\"$secretValue\" "
       fi
   done <<< "$(env | grep 'BUILD_SECRET_')"
 
   buildCommand="$buildCommand $buildArgs"
   echo "$buildCommand"
 }
-
 
 if [[ -z "${SP_USER}" ]]; then
   SP_USER=$(cat ${AZURE_CREDENTIALS} | jq -r '.id')
@@ -34,7 +35,6 @@ if [[ -z "${SP_SECRET}" ]]; then
   SP_SECRET=$(cat ${AZURE_CREDENTIALS} | jq -r '.password')
 fi
 
-echo "Here2"
 azBuildCommand=$(GetBuildCommand)
 
 az login --service-principal -u ${SP_USER} -p ${SP_SECRET} --tenant ${TENANT}
