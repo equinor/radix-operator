3acfc1515dfd0a253bddcadfb89ad154cb6269a8
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-12-16 15:13:42 +0100

    Remove unnecessary code

--- pipeline-runner/steps/build_test.go
+++ pipeline-runner/steps/build_test.go
@@ -1,7 +1,6 @@
 package steps
 
 import (
-	"fmt"
 	"testing"
 
 	monitoring "github.com/coreos/prometheus-operator/pkg/client/versioned"
@@ -11,7 +10,6 @@ import (
 	"github.com/equinor/radix-operator/pkg/apis/test"
 	commonTest "github.com/equinor/radix-operator/pkg/apis/test"
 	"github.com/equinor/radix-operator/pkg/apis/utils"
-	"github.com/equinor/radix-operator/pkg/apis/utils/git"
 	radix "github.com/equinor/radix-operator/pkg/client/clientset/versioned/fake"
 	kubernetes "k8s.io/client-go/kubernetes/fake"
 
@@ -71,54 +69,4 @@ func TestBuild_BranchIsNotMapped_ShouldSkip(t *testing.T) {
 	err := cli.Run(pipelineInfo)
 	assert.Error(t, err)
 
-}
-
-func Test_dockerfile_from_build_folder(t *testing.T) {
-	dockerfile := getDockerfile(".", "")
-
-	assert.Equal(t, fmt.Sprintf("%s/Dockerfile", git.Workspace), dockerfile)
-}
-
-func Test_dockerfile_from_folder(t *testing.T) {
-	dockerfile := getDockerfile("/afolder/", "")
-
-	assert.Equal(t, fmt.Sprintf("%s/afolder/Dockerfile", git.Workspace), dockerfile)
-}
-
-func Test_dockerfile_from_folder_2(t *testing.T) {
-	dockerfile := getDockerfile("afolder", "")
-
-	assert.Equal(t, fmt.Sprintf("%s/afolder/Dockerfile", git.Workspace), dockerfile)
-}
-
-func Test_dockerfile_from_folder_special_char(t *testing.T) {
-	dockerfile := getDockerfile("./afolder/", "")
-
-	assert.Equal(t, fmt.Sprintf("%s/afolder/Dockerfile", git.Workspace), dockerfile)
-}
-
-func Test_dockerfile_from_folder_and_file(t *testing.T) {
-	dockerfile := getDockerfile("/afolder/", "Dockerfile.adockerfile")
-
-	assert.Equal(t, fmt.Sprintf("%s/afolder/Dockerfile.adockerfile", git.Workspace), dockerfile)
-}
-
-func Test_getGitCloneCommand(t *testing.T) {
-	gitCloneCommand := getGitCloneCommand("git@github.com:equinor/radix-github-webhook.git", "master")
-
-	assert.Equal(t, "git clone git@github.com:equinor/radix-github-webhook.git -b master .", gitCloneCommand)
-}
-
-func Test_getInitContainerArgString_NoCommitID(t *testing.T) {
-	gitCloneCommand := getGitCloneCommand("git@github.com:equinor/radix-github-webhook.git", "master")
-	argString := getInitContainerArgString("/workspace", gitCloneCommand, "")
-
-	assert.Equal(t, "apk add --no-cache bash openssh-client git && ls /root/.ssh && cd /workspace && git clone git@github.com:equinor/radix-github-webhook.git -b master .", argString)
-}
-
-func Test_getInitContainerArgString_WithCommitID(t *testing.T) {
-	gitCloneCommand := getGitCloneCommand("git@github.com:equinor/radix-github-webhook.git", "master")
-	argString := getInitContainerArgString("/workspace", gitCloneCommand, "762235917d04dd541715df25a428b5f62394834c")
-
-	assert.Equal(t, "apk add --no-cache bash openssh-client git && ls /root/.ssh && cd /workspace && git clone git@github.com:equinor/radix-github-webhook.git -b master . && git checkout 762235917d04dd541715df25a428b5f62394834c", argString)
 }
