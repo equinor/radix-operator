02bd4f67a5ae30edd149a18ba21377cefea922a0
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-10-08 15:46:26 +0200

    Fixed test

--- pkg/apis/deployment/deployment_test.go
+++ pkg/apis/deployment/deployment_test.go
@@ -301,9 +301,9 @@ func TestObjectSynced_MultiComponent_ContainsAllElements(t *testing.T) {
 				}
 
 				if !componentsExist {
-					assert.True(t, secretByNameExists(fmt.Sprintf("%s-%s-blobfusecreds", componentNameRadixQuote, "blob"), secrets), "Volume mount secret")
+					assert.True(t, secretByNameExists(defaults.GetBlobFuseCredsSecret(componentNameRadixQuote), secrets), "Volume mount secret")
 				} else {
-					assert.False(t, secretByNameExists(fmt.Sprintf("%s-blobfusecreds", componentNameRadixQuote), secrets), "TLS certificate for external alias is not properly defined")
+					assert.False(t, secretByNameExists(defaults.GetBlobFuseCredsSecret(componentNameRadixQuote), secrets), "TLS certificate for external alias is not properly defined")
 				}
 			})
 
