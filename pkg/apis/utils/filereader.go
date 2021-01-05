a4a7146f3f4d9c51cb6ffb63de596c7d06427885
Sergey Smolnikov <ssmol@equinor.com>
2021-01-05 10:36:46 +0100

    Fixed unit-tests for environment

--- pkg/apis/utils/filereader.go
+++ pkg/apis/utils/filereader.go
@@ -6,65 +6,45 @@ import (
 
 	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 	log "github.com/sirupsen/logrus"
-	yaml "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 )
 
 // GetRadixApplication Reads radix config from file
 func GetRadixApplication(filename string) (*v1.RadixApplication, error) {
 	log.Debugf("get radix application yaml from %s", filename)
-	radixApp := v1.RadixApplication{}
-	yamlFile, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to read file %v Error:  %v ", filename, err)
-	}
-	err = yaml.Unmarshal(yamlFile, &radixApp)
-	if err != nil {
-		return nil, fmt.Errorf("Unmarshal: %v", err)
-	}
-
-	return &radixApp, nil
+	radixApp := &v1.RadixApplication{}
+	err := getFromFile(filename, radixApp)
+	return radixApp, err
 }
 
 // GetRadixRegistrationFromFile Reads radix registration from file
-func GetRadixRegistrationFromFile(file string) (*v1.RadixRegistration, error) {
-	raw, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, err
-	}
+func GetRadixRegistrationFromFile(filename string) (*v1.RadixRegistration, error) {
+	log.Debugf("get radix registration yaml from %s", filename)
 	reg := &v1.RadixRegistration{}
-	err = yaml.Unmarshal(raw, reg)
-	if err != nil {
-		return nil, err
-	}
-	return reg, nil
+	err := getFromFile(filename, reg)
+	return reg, err
 }
 
 // GetRadixDeploy Reads radix deployment from file
 func GetRadixDeploy(filename string) (*v1.RadixDeployment, error) {
 	log.Debugf("get radix deploy yaml from %s", filename)
-	radixDeploy := v1.RadixDeployment{}
-	yamlFile, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to read file %v Error:  %v ", filename, err)
-	}
-	err = yaml.Unmarshal(yamlFile, &radixDeploy)
-	if err != nil {
-		return nil, fmt.Errorf("Unmarshal: %v", err)
-	}
-
-	return &radixDeploy, nil
+	radixDeploy := &v1.RadixDeployment{}
+	err := getFromFile(filename, radixDeploy)
+	return radixDeploy, err
 }
 
-// GetRadixEnvironment reads RadixEnvironment configuration from a yaml file
-func GetRadixEnvironment(file string) (*v1.RadixEnvironment, error) {
-	raw, err := ioutil.ReadFile(file)
+// GetRadixEnvironmentFromFile reads RadixEnvironment configuration from a yaml file
+func GetRadixEnvironmentFromFile(filename string) (*v1.RadixEnvironment, error) {
+	log.Debugf("get radix environment yaml from %s", filename)
+	env := &v1.RadixEnvironment{}
+	err := getFromFile(filename, env)
+	return env, err
+}
+
+func getFromFile(filename string, objRef interface{}) error {
+	raw, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("Failed to read file %v Error: %v ", filename, err)
 	}
-	reg := &v1.RadixEnvironment{}
-	err = yaml.Unmarshal(raw, reg)
-	if err != nil {
-		return nil, err
-	}
-	return reg, nil
+	return yaml.Unmarshal(raw, objRef)
 }
