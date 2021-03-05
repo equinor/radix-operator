9b75ef807b4c44abae482f8968c0c0e5e6f118d7
Sergey Smolnikov <ssmol@equinor.com>
2021-03-05 14:33:46 +0100

    Added BoolPtr

--- pkg/apis/utils/strings.go
+++ pkg/apis/utils/strings.go
@@ -42,3 +42,8 @@ func TernaryString(condition bool, trueValue, falseValue string) string {
 func StringPtr(s string) *string {
 	return &s
 }
+
+// BoolPtr returns a pointer to the passed bool.
+func BoolPtr(value bool) *bool {
+	return &value
+}
