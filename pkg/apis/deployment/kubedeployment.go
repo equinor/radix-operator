c5ecb429450e9f9daee431ce99d9cd25c3ca9b63
Sergey Smolnikov <ssmol@equinor.com>
2021-06-18 15:22:12 +0200

    Fixed create deploy

--- pkg/apis/deployment/kubedeployment.go
+++ pkg/apis/deployment/kubedeployment.go
@@ -160,6 +160,8 @@ func (deploy *Deployment) setDesiredDeploymentProperties(deployComponent v1.Radi
 
 	desiredDeployment.Spec.Selector.MatchLabels[kube.RadixComponentLabel] = componentName
 
+	desiredDeployment.Spec.Template.ObjectMeta.Labels[kube.RadixAppLabel] = appName
+	desiredDeployment.Spec.Template.ObjectMeta.Labels[kube.RadixComponentLabel] = componentName
 	desiredDeployment.Spec.Template.ObjectMeta.Labels[kube.RadixCommitLabel] = commitID
 	desiredDeployment.Spec.Template.ObjectMeta.Annotations["apparmor.security.beta.kubernetes.io/pod"] = "runtime/default"
 	desiredDeployment.Spec.Template.ObjectMeta.Annotations["seccomp.security.alpha.kubernetes.io/pod"] = "docker/default"
