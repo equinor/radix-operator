8adefc1b275e26564c9765f647b5a6999420f111
Sergey Smolnikov <ssmol@equinor.com>
2021-10-13 09:31:47 +0200

    128307 update rolebindings on change admin or machine user (#611)
    * Added radix-registration update
    
    * Added test suite
    
    * Added test for updating RR
    
    * Added test for updating RR
    
    * Generalised controller test infra
    
    * Added common mock
    
    * Removed outdated argument
    
    * Added comments
    
    * Extracted common logic

--- radix-operator/application/controller_test.go
+++ radix-operator/application/controller_test.go
@@ -2,21 +2,13 @@ package application
 
 import (
 	"context"
-	"testing"
-
-	"github.com/equinor/radix-operator/pkg/apis/kube"
+	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 	"github.com/equinor/radix-operator/pkg/apis/test"
 	"github.com/equinor/radix-operator/pkg/apis/utils"
-	radixclient "github.com/equinor/radix-operator/pkg/client/clientset/versioned"
-	fakeradix "github.com/equinor/radix-operator/pkg/client/clientset/versioned/fake"
-	informers "github.com/equinor/radix-operator/pkg/client/informers/externalversions"
-	"github.com/stretchr/testify/assert"
-	corev1 "k8s.io/api/core/v1"
+	"github.com/equinor/radix-operator/radix-operator/common"
+	"github.com/stretchr/testify/suite"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kubeinformers "k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/fake"
-	"k8s.io/client-go/tools/record"
+	"testing"
 )
 
 const (
@@ -25,84 +17,62 @@ const (
 	containerRegistry = "any.container.registry"
 )
 
-var synced chan bool
-
-func setupTest() (*test.Utils, kubernetes.Interface, *kube.Kube, radixclient.Interface) {
-	client := fake.NewSimpleClientset()
-	radixClient := fakeradix.NewSimpleClientset()
-	kubeUtil, _ := kube.New(client, radixClient)
-
-	handlerTestUtils := test.NewTestUtils(client, radixClient)
-	handlerTestUtils.CreateClusterPrerequisites(clusterName, containerRegistry)
-	return &handlerTestUtils, client, kubeUtil, radixClient
+type controllerTestSuite struct {
+	common.ControllerTestSuite
 }
 
-func Test_Controller_Calls_Handler(t *testing.T) {
-	anyAppName := "test-app"
-
-	// Setup
-	tu, client, kubeUtil, radixClient := setupTest()
-
-	client.CoreV1().Namespaces().Create(
-		context.TODO(),
-		&corev1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: utils.GetAppNamespace(anyAppName),
-				Labels: map[string]string{
-					kube.RadixAppLabel: anyAppName,
-					kube.RadixEnvLabel: "app",
-				},
-			},
-		},
-		metav1.CreateOptions{})
-
-	stop := make(chan struct{})
-	synced := make(chan bool)
-
-	defer close(stop)
-	defer close(synced)
-
-	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(client, 0)
-	radixInformerFactory := informers.NewSharedInformerFactory(radixClient, 0)
-
-	applicationHandler := NewHandler(
-		client,
-		kubeUtil,
-		radixClient,
-		func(syncedOk bool) {
-			synced <- syncedOk
-		},
-	)
-	go startApplicationController(client, kubeUtil, radixClient, radixInformerFactory, kubeInformerFactory, applicationHandler, stop)
-
-	// Test
-
-	// Create registration should sync
-	tu.ApplyApplication(
-		utils.ARadixApplication().
-			WithAppName(anyAppName).
-			WithEnvironment("dev", "master"))
-
-	op, ok := <-synced
-	assert.True(t, ok)
-	assert.True(t, op)
+func TestControllerSuite(t *testing.T) {
+	suite.Run(t, new(controllerTestSuite))
 }
 
-func startApplicationController(
-	client kubernetes.Interface,
-	kubeutil *kube.Kube,
-	radixClient radixclient.Interface,
-	radixInformerFactory informers.SharedInformerFactory,
-	kubeInformerFactory kubeinformers.SharedInformerFactory,
-	handler Handler, stop chan struct{}) {
-	eventRecorder := &record.FakeRecorder{}
+func (s *controllerTestSuite) Test_Controller_Calls_Handler() {
+	appName := "any-app"
+	namespace := utils.GetAppNamespace(appName)
+	appNamespace := test.CreateAppNamespace(s.KubeClient, appName)
 
-	waitForChildrenToSync := false
-	controller := NewController(client, kubeutil, radixClient, &handler,
-		kubeInformerFactory, radixInformerFactory, waitForChildrenToSync, eventRecorder)
+	sut := NewController(s.KubeClient, s.KubeUtil, s.RadixClient, s.Handler, s.KubeInformerFactory, s.RadixInformerFactory, false, s.EventRecorder)
+	s.RadixInformerFactory.Start(s.Stop)
+	s.KubeInformerFactory.Start(s.Stop)
 
-	kubeInformerFactory.Start(stop)
-	radixInformerFactory.Start(stop)
-	controller.Run(1, stop)
+	go sut.Run(1, s.Stop)
 
+	ra := utils.ARadixApplication().WithAppName(appName).WithEnvironment("dev", "master").BuildRA()
+	s.RadixClient.RadixV1().RadixApplications(appNamespace).Create(context.TODO(), ra, metav1.CreateOptions{})
+
+	s.Handler.EXPECT().Sync(namespace, appName, s.EventRecorder).DoAndReturn(s.SyncedChannelCallback()).Times(1)
+	s.WaitForSynced("added app")
+}
+
+func (s *controllerTestSuite) Test_Controller_Calls_Handler_On_Admin_Or_MachineUser_Change() {
+	appName := "any-app"
+	namespace := utils.GetAppNamespace(appName)
+	appNamespace := test.CreateAppNamespace(s.KubeClient, appName)
+	rr := &v1.RadixRegistration{ObjectMeta: metav1.ObjectMeta{Name: appName}, Spec: v1.RadixRegistrationSpec{MachineUser: false, AdGroups: []string{"first-group"}}}
+	rr, _ = s.RadixClient.RadixV1().RadixRegistrations().Create(context.TODO(), rr, metav1.CreateOptions{})
+
+	sut := NewController(s.KubeClient, s.KubeUtil, s.RadixClient, s.Handler, s.KubeInformerFactory, s.RadixInformerFactory, false, s.EventRecorder)
+	s.RadixInformerFactory.Start(s.Stop)
+	s.KubeInformerFactory.Start(s.Stop)
+
+	go sut.Run(1, s.Stop)
+
+	ra := utils.ARadixApplication().WithAppName(appName).WithEnvironment("dev", "master").BuildRA()
+	s.RadixClient.RadixV1().RadixApplications(appNamespace).Create(context.TODO(), ra, metav1.CreateOptions{})
+	s.Handler.EXPECT().Sync(namespace, appName, s.EventRecorder).DoAndReturn(s.SyncedChannelCallback()).Times(1)
+	s.WaitForSynced("added app")
+
+	rr.Spec.MachineUser = true
+	rr, _ = s.RadixClient.RadixV1().RadixRegistrations().Update(context.TODO(), rr, metav1.UpdateOptions{})
+	s.Handler.EXPECT().Sync(namespace, appName, s.EventRecorder).DoAndReturn(s.SyncedChannelCallback()).Times(1)
+	s.WaitForSynced("set machine-user")
+
+	rr.Spec.MachineUser = false
+	rr, _ = s.RadixClient.RadixV1().RadixRegistrations().Update(context.TODO(), rr, metav1.UpdateOptions{})
+	s.Handler.EXPECT().Sync(namespace, appName, s.EventRecorder).DoAndReturn(s.SyncedChannelCallback()).Times(1)
+	s.WaitForSynced("unset machine-user")
+
+	rr.Spec.AdGroups = []string{"another-group"}
+	rr, _ = s.RadixClient.RadixV1().RadixRegistrations().Update(context.TODO(), rr, metav1.UpdateOptions{})
+	s.Handler.EXPECT().Sync(namespace, appName, s.EventRecorder).DoAndReturn(s.SyncedChannelCallback()).Times(1)
+	s.WaitForSynced("unset machine-user")
 }
