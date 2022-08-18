64e0d55b346e45de7bd8db31d3b883ecef40f566
Nils Gustav Stråbø <nst@equinor.com>
2022-08-18 10:48:29 +0200

    implement labels for podsecuritystandard

--- pkg/apis/application/namespace.go
+++ pkg/apis/application/namespace.go
@@ -3,6 +3,7 @@ package application
 import (
 	"github.com/equinor/radix-operator/pkg/apis/kube"
 	"github.com/equinor/radix-operator/pkg/apis/utils"
+	"k8s.io/apimachinery/pkg/labels"
 )
 
 // createAppNamespace creates an app namespace with RadixRegistration as owner
@@ -10,14 +11,15 @@ func (app Application) createAppNamespace() error {
 	registration := app.registration
 	name := utils.GetAppNamespace(registration.Name)
 
-	labels := map[string]string{
+	nsLabels := map[string]string{
 		kube.RadixAppLabel:          registration.Name,
 		kube.RadixEnvLabel:          utils.AppNamespaceEnvName,
 		"snyk-service-account-sync": "radix-snyk-service-account",
 	}
+	nsLabels = labels.Merge(nsLabels, labels.Set(kube.NewPodSecurityStandardFromEnv().Labels()))
 
 	ownerRef := app.getOwnerReference()
-	err := app.kubeutil.ApplyNamespace(name, labels, ownerRef)
+	err := app.kubeutil.ApplyNamespace(name, nsLabels, ownerRef)
 
 	if err != nil {
 		logger.Errorf("Failed to create namespace %s: %v", name, err)
---
+++ pkg/apis/kube/podsecuritystandard.go
@@ -0,0 +1,100 @@
+package kube
+
+import (
+	"os"
+	"strings"
+
+	"github.com/equinor/radix-operator/pkg/apis/defaults"
+)
+
+type podSecurityMode string
+type PodSecurityLevel string
+
+const (
+	modeEnforce podSecurityMode = "enforce"
+	modeAudit   podSecurityMode = "audit"
+	modeWarn    podSecurityMode = "warn"
+
+	PrivilegedLevel PodSecurityLevel = "privileged"
+	BaselineLevel   PodSecurityLevel = "baseline"
+	RestrictedLevel PodSecurityLevel = "restricted"
+
+	podSecurityStandardLabelPrefix = "pod-security.kubernetes.io/"
+)
+
+// NewPodSecurityStandardFromEnv builds pod security standard from environment variables
+func NewPodSecurityStandardFromEnv() *PodSecurityStandard {
+	pss := &PodSecurityStandard{}
+
+	// Set Enforce level
+	if level := os.Getenv(defaults.PodSecurityStandardEnforceLevelEnvironmentVariable); len(level) > 0 {
+		version := os.Getenv(defaults.PodSecurityStandardEnforceVersionEnvironmentVariable)
+		pss.Enforce(PodSecurityLevel(level), version)
+	}
+
+	// Set Audit level
+	if level := os.Getenv(defaults.PodSecurityStandardAuditLevelEnvironmentVariable); len(level) > 0 {
+		version := os.Getenv(defaults.PodSecurityStandardAuditVersionEnvironmentVariable)
+		pss.Audit(PodSecurityLevel(level), version)
+	}
+
+	// Set Warn level
+	if level := os.Getenv(defaults.PodSecurityStandardWarnLevelEnvironmentVariable); len(level) > 0 {
+		version := os.Getenv(defaults.PodSecurityStandardWarnVersionEnvironmentVariable)
+		pss.Warn(PodSecurityLevel(level), version)
+	}
+
+	return pss
+}
+
+type podSecurityModeSpec struct {
+	version string
+	level   PodSecurityLevel
+}
+
+// PodSecurityStandard defines methods to build pod security standard labels. See https://kubernetes.io/docs/concepts/security/pod-security-standards/
+type PodSecurityStandard struct {
+	mode map[podSecurityMode]podSecurityModeSpec
+}
+
+// Enforce policy
+// Policy violations will cause the pod to be rejected.
+func (pss *PodSecurityStandard) Enforce(level PodSecurityLevel, version string) *PodSecurityStandard {
+	pss.setMode(modeEnforce, level, version)
+	return pss
+}
+
+// Audit pod policy violations.
+// Policy violations will trigger the addition of an audit annotation to the event recorded in the audit log, but are otherwise allowed.
+func (pss *PodSecurityStandard) Audit(level PodSecurityLevel, version string) *PodSecurityStandard {
+	pss.setMode(modeAudit, level, version)
+	return pss
+}
+
+// Warn triggers a user-facing warning (e.g. kubectl) when a pod violates the policy
+func (pss *PodSecurityStandard) Warn(level PodSecurityLevel, version string) *PodSecurityStandard {
+	pss.setMode(modeWarn, level, version)
+	return pss
+}
+
+// Labels returns labels that will enforce pod security standard when applied on a namespace
+func (pss *PodSecurityStandard) Labels() map[string]string {
+	labels := make(map[string]string)
+	for mode, spec := range pss.mode {
+		version := spec.version
+		if len(strings.TrimSpace(version)) == 0 {
+			version = "latest"
+		}
+
+		labels[podSecurityStandardLabelPrefix+string(mode)] = string(spec.level)
+		labels[podSecurityStandardLabelPrefix+string(mode)+"-version"] = version
+	}
+	return labels
+}
+
+func (pss *PodSecurityStandard) setMode(mode podSecurityMode, level PodSecurityLevel, version string) {
+	if pss.mode == nil {
+		pss.mode = make(map[podSecurityMode]podSecurityModeSpec)
+	}
+	pss.mode[mode] = podSecurityModeSpec{level: level, version: version}
+}
