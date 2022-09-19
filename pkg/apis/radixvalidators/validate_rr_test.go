545b1768e5174f31ed3f7ed396015910d1f91de4
Sergey Smolnikov <ssmol@equinor.com>
2022-09-19 15:25:28 +0200

    Allowed to insert/update an app with the already used repo

--- pkg/apis/radixvalidators/validate_rr_test.go
+++ pkg/apis/radixvalidators/validate_rr_test.go
@@ -1,6 +1,7 @@
 package radixvalidators_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -17,9 +18,17 @@ import (
 func Test_valid_rr_returns_true(t *testing.T) {
 	_, client := validRRSetup()
 	validRR := createValidRR()
-	isValid, err := radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
+	err := radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
 
-	assert.True(t, isValid)
+	assert.Nil(t, err)
+}
+
+func Test_valid_rr_returns_no_warnings(t *testing.T) {
+	_, client := validRRSetup()
+	validRR := createValidRR()
+	_ = radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
+	warnings, err := radixvalidators.GetRadixRegistrationBeInsertedWarnings(client, validRR)
+	assert.Empty(t, warnings)
 	assert.Nil(t, err)
 }
 
@@ -60,9 +69,8 @@ func TestCanRadixApplicationBeInserted(t *testing.T) {
 		t.Run(testcase.name, func(t *testing.T) {
 			validRR := createValidRR()
 			testcase.updateRR(validRR)
-			isValid, err := radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
+			err := radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
 
-			assert.False(t, isValid)
 			assert.NotNil(t, err)
 		})
 	}
@@ -70,21 +78,87 @@ func TestCanRadixApplicationBeInserted(t *testing.T) {
 	t.Run("name already exist", func(t *testing.T) {
 		validRR := createValidRR()
 		client = radixfake.NewSimpleClientset(validRR)
-		isValid, err := radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
+		err := radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
 
-		assert.False(t, isValid)
 		assert.NotNil(t, err)
 	})
 
 	t.Run("repo already used by other app", func(t *testing.T) {
 		validRR := createValidRR()
+		existingAppName := validRR.GetName()
 		client = radixfake.NewSimpleClientset(validRR)
 		validRR = createValidRR()
 		validRR.Name = "new-app"
-		isValid, err := radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
+		err := radixvalidators.CanRadixRegistrationBeInserted(client, validRR)
 
-		assert.False(t, isValid)
-		assert.NotNil(t, err)
+		assert.Nil(t, err)
+		warnings, err := radixvalidators.GetRadixRegistrationBeInsertedWarnings(client, validRR)
+		assert.NotEmpty(t, warnings)
+		assert.Equal(t, fmt.Sprintf("Repository is in use by %s", existingAppName), warnings[0])
+	})
+}
+
+func TestCanRadixApplicationBeUpdated(t *testing.T) {
+	var testScenarios = []struct {
+		name     string
+		updateRR updateRRFunc
+	}{
+		{"to long app name", func(rr *v1.RadixRegistration) {
+			rr.Name = "way.toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo.long-app-name"
+		}},
+		{"invalid app name", func(rr *v1.RadixRegistration) { rr.Name = "invalid,char.appname" }},
+		{"empty app name", func(rr *v1.RadixRegistration) { rr.Name = "" }},
+		{"empty WBS", func(rr *v1.RadixRegistration) { rr.Spec.WBS = "" }},
+		{"empty WBS", func(rr *v1.RadixRegistration) { rr.Spec.WBS = " " }},
+		{"WBS is too short", func(rr *v1.RadixRegistration) { rr.Spec.WBS = strings.Repeat("a", 4) }},
+		{"WBS is too long", func(rr *v1.RadixRegistration) { rr.Spec.WBS = strings.Repeat("a", 101) }},
+		{"invalid owner email", func(rr *v1.RadixRegistration) { rr.Spec.Owner = "radix@equinor_com" }},
+		{"invalid owner email", func(rr *v1.RadixRegistration) { rr.Spec.Owner = "radixatequinor.com" }},
+		{"invalid owner email", func(rr *v1.RadixRegistration) { rr.Spec.Owner = "adfasd" }},
+		{"require owner email", func(rr *v1.RadixRegistration) { rr.Spec.Owner = "" }},
+		{"invalid ssh url ending", func(rr *v1.RadixRegistration) { rr.Spec.CloneURL = "git@github.com:auser/go-roman.gitblabla" }},
+		{"invalid ssh url start", func(rr *v1.RadixRegistration) { rr.Spec.CloneURL = "asdfasdgit@github.com:auser/go-roman.git" }},
+		{"invalid ssh url https", func(rr *v1.RadixRegistration) { rr.Spec.CloneURL = "https://github.com/auser/go-roman" }},
+		{"empty ssh url", func(rr *v1.RadixRegistration) { rr.Spec.CloneURL = "" }},
+		{"invalid ad group lenght", func(rr *v1.RadixRegistration) { rr.Spec.AdGroups = []string{"7552642f-asdff-fs43-23sf-3ab8f3742c16"} }},
+		{"invalid ad group name", func(rr *v1.RadixRegistration) { rr.Spec.AdGroups = []string{"fg_some_group_name"} }},
+		{"empty ad group", func(rr *v1.RadixRegistration) { rr.Spec.AdGroups = []string{""} }},
+		{"empty configBranch", func(rr *v1.RadixRegistration) { rr.Spec.ConfigBranch = "" }},
+		{"invalid configBranch", func(rr *v1.RadixRegistration) { rr.Spec.ConfigBranch = "main.." }},
+	}
+
+	_, client := validRRSetup()
+
+	for _, testcase := range testScenarios {
+		t.Run(testcase.name, func(t *testing.T) {
+			validRR := createValidRR()
+			testcase.updateRR(validRR)
+			err := radixvalidators.CanRadixRegistrationBeUpdated(validRR)
+
+			assert.NotNil(t, err)
+		})
+	}
+
+	t.Run("name already exist", func(t *testing.T) {
+		validRR := createValidRR()
+		client = radixfake.NewSimpleClientset(validRR)
+		err := radixvalidators.CanRadixRegistrationBeUpdated(validRR)
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("repo already used by other app", func(t *testing.T) {
+		validRR := createValidRR()
+		existingAppName := validRR.GetName()
+		client = radixfake.NewSimpleClientset(validRR)
+		validRR = createValidRR()
+		validRR.Name = "new-app"
+		err := radixvalidators.CanRadixRegistrationBeUpdated(validRR)
+
+		assert.Nil(t, err)
+		warnings, err := radixvalidators.GetRadixRegistrationBeInsertedWarnings(client, validRR)
+		assert.NotEmpty(t, warnings)
+		assert.Equal(t, fmt.Sprintf("Repository is in use by %s", existingAppName), warnings[0])
 	})
 }
 
