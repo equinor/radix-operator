9d63d85e74a6d716b12e656068cf299f593e0542
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2022-12-06 13:31:51 +0100

    Update pkg/apis/utils/labels/labels_test.go
    Co-authored-by: Sergey Smolnikov <ssmol@equinor.com>

--- pkg/apis/utils/labels/labels_test.go
+++ pkg/apis/utils/labels/labels_test.go
@@ -50,7 +50,7 @@ func Test_ForServiceAccountWithRadixIdentity(t *testing.T) {
 	assert.Equal(t, kubelabels.Set(nil), actual)
 
 	actual = ForServiceAccountWithRadixIdentity(&v1.Identity{Azure: &v1.AzureIdentity{ClientId: "any"}})
-	expected := kubelabels.Set{"azure.workload.identity/use": "true"}
+	expected := kubelabels.Set{azureWorkloadIdentityUseLabel: "true"}
 	assert.Equal(t, expected, actual)
 }
 
