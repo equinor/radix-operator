c81a09315bbebeb670ee19bed01682f0b5d9b04d
Nils Gustav Stråbø <nst@equinor.com>
2022-12-12 15:27:59 +0100

    radixscheduledjob spec

--- pkg/apis/radix/v1/radixscheduledjobtypes.go
+++ pkg/apis/radix/v1/radixscheduledjobtypes.go
@@ -24,8 +24,36 @@ type RadixScheduledJobList struct {
 	Items            []RadixScheduledJob `json:"items" yaml:"items"`
 }
 
+type LocalObjectReference struct {
+	// Name of the resource being referred to.
+	// More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
+	Name string `json:"name"`
+}
+
 // RadixScheduledJobSpec is the spec for a RadixScheduledJob
 type RadixScheduledJobSpec struct {
+	JobId                 string                              `json:"jobId,omitempty"`
+	Resources             *ResourceRequirements               `json:"resources,omitempty"`
+	Node                  *RadixNode                          `json:"node,omitempty"`
+	TimeLimitSeconds      *int64                              `json:"timeLimitSeconds,omitempty"`
+	RadixDeploymentJobRef RadixDeploymentJobComponentSelector `json:"radixDeploymentJobRef"`
+	PayloadSecretRef      *PayloadSecretKeySelector           `json:"payloadSecretRef,omitempty"`
+}
+
+type PayloadSecretKeySelector struct {
+	// The name of the Secret resource being referred to.
+	LocalObjectReference `json:",inline"`
+
+	// The key of the entry in the Secret resource's `data` field to be used.
+	Key string `json:"key"`
+}
+
+// A reference to a specific job within a RadixDeployment resource.
+type RadixDeploymentJobComponentSelector struct {
+	// The name of the RadixDeployment resource being referred to
+	LocalObjectReference `json:",inline"`
+
+	Job string `json:"job"`
 }
 
 // RadixScheduledJobStatus is the status for a RadixScheduledJob
