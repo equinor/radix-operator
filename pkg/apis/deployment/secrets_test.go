47837f3c8e970c1281107f526dea8f4bd260e34d
Sergey Smolnikov <ssmol@equinor.com>
2023-01-12 15:23:14 +0100

    Added unit-test (#819)

--- pkg/apis/deployment/secrets_test.go
+++ pkg/apis/deployment/secrets_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/equinor/radix-common/utils/pointers"
 	"github.com/equinor/radix-operator/pkg/apis/defaults"
 	"github.com/equinor/radix-operator/pkg/apis/kube"
 	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
@@ -128,4 +129,35 @@ func TestSecretDeployed_ClientCertificateSecretGetsSet(t *testing.T) {
 	})
 
 	teardownSecretsTest()
+}
+
+func Test_IsSecretRequiredForClientCertificate(t *testing.T) {
+	type scenario struct {
+		clientCertificate *v1.ClientCertificate
+		expectedResult    bool
+	}
+	scenarios := []scenario{
+		{clientCertificate: nil, expectedResult: false},
+		{clientCertificate: &v1.ClientCertificate{Verification: getVerificationTypePtr(v1.VerificationTypeOff)}, expectedResult: false},
+		{clientCertificate: &v1.ClientCertificate{Verification: getVerificationTypePtr(v1.VerificationTypeOptional)}, expectedResult: true},
+		{clientCertificate: &v1.ClientCertificate{Verification: getVerificationTypePtr(v1.VerificationTypeOptionalNoCa)}, expectedResult: true},
+		{clientCertificate: &v1.ClientCertificate{Verification: getVerificationTypePtr(v1.VerificationTypeOn)}, expectedResult: true},
+		{clientCertificate: &v1.ClientCertificate{}, expectedResult: false},
+		{clientCertificate: &v1.ClientCertificate{PassCertificateToUpstream: pointers.Ptr(false)}, expectedResult: false},
+		{clientCertificate: &v1.ClientCertificate{PassCertificateToUpstream: pointers.Ptr(true)}, expectedResult: true},
+		{clientCertificate: &v1.ClientCertificate{Verification: getVerificationTypePtr(v1.VerificationTypeOff), PassCertificateToUpstream: pointers.Ptr(true)}, expectedResult: true},
+		{clientCertificate: &v1.ClientCertificate{Verification: getVerificationTypePtr(v1.VerificationTypeOff), PassCertificateToUpstream: pointers.Ptr(false)}, expectedResult: false},
+		{clientCertificate: &v1.ClientCertificate{Verification: getVerificationTypePtr(v1.VerificationTypeOptional), PassCertificateToUpstream: pointers.Ptr(true)}, expectedResult: true},
+		{clientCertificate: &v1.ClientCertificate{Verification: getVerificationTypePtr(v1.VerificationTypeOptional), PassCertificateToUpstream: pointers.Ptr(false)}, expectedResult: true},
+	}
+
+	for _, ts := range scenarios {
+		t.Run("", func(t *testing.T) {
+			assert.Equal(t, ts.expectedResult, IsSecretRequiredForClientCertificate(ts.clientCertificate))
+		})
+	}
+}
+
+func getVerificationTypePtr(verificationType v1.VerificationType) *v1.VerificationType {
+	return &verificationType
 }
