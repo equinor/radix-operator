bfeafe2cb00ed70a8c9beb2bafa7ef37e76cc560
Nils Gustav Stråbø <nst@equinor.com>
2023-01-25 16:40:21 +0100

    add comments to radixbatch type

--- pkg/apis/radix/v1/radixbatchtypes.go
+++ pkg/apis/radix/v1/radixbatchtypes.go
@@ -12,7 +12,7 @@ import (
 // +kubebuilder:resource:path=radixbatches,shortName=rb
 // +kubebuilder:subresource:status
 
-// RadixBatch describes a job
+// RadixBatch enables batch execution of Radix job components.
 type RadixBatch struct {
 	meta_v1.TypeMeta   `json:",inline"`
 	meta_v1.ObjectMeta `json:"metadata"`
@@ -23,7 +23,7 @@ type RadixBatch struct {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// RadixBatchList is a list of RadixBatch
+// RadixBatchList is a collection of RadixBatches.
 type RadixBatchList struct {
 	meta_v1.TypeMeta `json:",inline"`
 	meta_v1.ListMeta `json:"metadata"`
@@ -31,9 +31,12 @@ type RadixBatchList struct {
 	Items []RadixBatch `json:"items"`
 }
 
+// RadixBatchSpec is the specification of batch jobs.
 type RadixBatchSpec struct {
+	// Reference to the RadixDeployment containing the job component spec.
 	RadixDeploymentJobRef RadixDeploymentJobComponentSelector `json:"radixDeploymentJobRef"`
 
+	// List of batch jobs to run.
 	// +listType:=map
 	// +listMapKey:=name
 	// +kubebuilder:validation:MinItems:=1
@@ -41,50 +44,63 @@ type RadixBatchSpec struct {
 	Jobs []RadixBatchJob `json:"jobs"`
 }
 
-// RadixBatchJob defines the spec for a single job in the batch
+// Spec for a batch job
 type RadixBatchJob struct {
+	// Defines the unique name of the job in a RadixBatch.
 	// +kubebuilder:validation:MaxLength:=63
 	Name string `json:"name"`
 
+	// Defines a user defined ID of the job.
 	// +optional
 	JobId string `json:"jobId,omitempty"`
 
+	// Specifies compute resource requirements.
+	// Overrides resource configuration defined for job component in RadixDeployment.
 	// +optional
 	Resources *ResourceRequirements `json:"resources,omitempty"`
 
+	// Specifies node attributes, where container should be scheduled.
+	// Overrides node configuration defined for job component in RadixDeployment.
 	// +optional
 	Node *RadixNode `json:"node,omitempty"`
 
+	// Specifies maximum job run time.
+	// Overrides timeLimitSeconds defined for job component in RadixDeployment.
 	// +optional
 	// +kubebuilder:validation:Minimum:=1
 	TimeLimitSeconds *int64 `json:"timeLimitSeconds,omitempty"`
 
+	// Specifies the number of retries before marking this job failed.
 	// +optional
 	// +kubebuilder:validation:Minimum:=0
 	BackoffLimit *int32 `json:"backoffLimit,omitempty"`
 
+	// Specifies the Secret name and data key containing the payload for the job
 	// +optional
 	PayloadSecretRef *PayloadSecretKeySelector `json:"payloadSecretRef,omitempty"`
 
+	// Controls if a job should be stopped.
+	// If Stop is set to true, the underlying Kubernetes Job is deleted.
+	// A job that is stopped cannot be started again by setting Stop to false.
 	// +optional
 	Stop *bool `json:"stop,omitempty"`
 }
 
-// PayloadSecretKeySelector selects a Secret to get job payload from
+// PayloadSecretKeySelector selects a key of a Secret.
 type PayloadSecretKeySelector struct {
-	// The name of the Secret resource being referred to.
+	// The name of the secret in the RadixBatch namespace to select from.
 	LocalObjectReference `json:",inline"`
 
-	// The key of the entry in the Secret resource's `data` field to be used.
+	// The key of the secret to select from.
 	Key string `json:"key"`
 }
 
-// A reference to a specific job within a RadixDeployment resource.
+// RadixDeploymentJobComponentSelector selects a job component of a RadixDeployment.
 type RadixDeploymentJobComponentSelector struct {
-	// The name of the RadixDeployment resource being referred to
+	// The name of the RadixDeployment in the RadixBatch namespace to select from.
 	LocalObjectReference `json:",inline"`
 
-	// The name of the job in the RadixDeployment's `jobs` list to be used
+	// The job name of the RadixDeployment to select.
 	Job string `json:"job"`
 }
 
@@ -93,20 +109,23 @@ type RadixDeploymentJobComponentSelector struct {
 type RadixBatchJobPhase string
 
 const (
-	// BatchJobPhaseWaiting means that the the job is waiting to start
-	// Details about the reason for this state is found in the `reason` field
+	// Waiting means that the the job is waiting to start,
+	// either because the Kubernetes job has not yet been created,
+	// or the Kubernetes job controller has not processed the Job.
 	BatchJobPhaseWaiting RadixBatchJobPhase = "Waiting"
 
-	// BatchJobPhaseActive means that the job is active
+	// Active means that the job is active.
+	// The Kubernetes job is created, and the Kubernetes job
+	// controller has started the job.
 	BatchJobPhaseActive RadixBatchJobPhase = "Active"
 
-	// BatchJobPhaseSucceeded means that the job has completed without errors
+	// Succeeded means that the job has completed without errors.
 	BatchJobPhaseSucceeded RadixBatchJobPhase = "Succeeded"
 
-	// BatchJobPhaseFailed means that the job has failed
+	// Failed means that the job has failed.
 	BatchJobPhaseFailed RadixBatchJobPhase = "Failed"
 
-	// BatchJobPhaseStopped means that the job has been stopped
+	// Stopped means that the job has been stopped.
 	BatchJobPhaseStopped RadixBatchJobPhase = "Stopped"
 )
 
@@ -114,28 +133,39 @@ const (
 type RadixBatchConditionType string
 
 const (
-	BatchConditionTypeWaiting   RadixBatchConditionType = "Waiting"
-	BatchConditionTypeActive    RadixBatchConditionType = "Active"
+	// Waiting means that all jobs are in phase Waiting.
+	BatchConditionTypeWaiting RadixBatchConditionType = "Waiting"
+
+	// Active means that one or more jobs are in phase Active.
+	BatchConditionTypeActive RadixBatchConditionType = "Active"
+
+	// Completed means that all jobs are in Succeeded, Failed or Stopped phase.
 	BatchConditionTypeCompleted RadixBatchConditionType = "Completed"
 )
 
+// RadixBatchCondition describes the state of the RadixBatch
 type RadixBatchCondition struct {
+	// Type of RadixBatch condition.
 	Type RadixBatchConditionType `json:"type"`
 
+	// The reason for the condition.
 	// +optional
 	Reason string `json:"reason,omitempty"`
 
+	// A human readable message indicating details about the condition.
 	// +optional
 	Message string `json:"message,omitempty"`
 
+	// The time the condition entered Active state.
 	// +optional
 	ActiveTime *meta_v1.Time `json:"activeTime,omitempty"`
 
+	// The time the condition entered Completed state.
 	// +optional
 	CompletionTime *meta_v1.Time `json:"completionTime,omitempty"`
 }
 
-// RadixBatchStatus is the status for a RadixBatch
+// RadixBatchStatus represents the current state of a RadixBatch
 type RadixBatchStatus struct {
 	// Status for each job defined in spec.jobs
 	// +optional
@@ -146,7 +176,7 @@ type RadixBatchStatus struct {
 	Condition RadixBatchCondition `json:"condition,omitempty"`
 }
 
-// RadixBatchJobStatus is the status for a specific item in spec.items
+// RadixBatchJobStatus contains details for the current status of the job.
 type RadixBatchJobStatus struct {
 	// +kubebuilder:validation:MaxLength:=63
 	Name string `json:"name"`
@@ -161,18 +191,27 @@ type RadixBatchJobStatus struct {
 	// +optional
 	Message string `json:"message,omitempty"`
 
+	// The time at which the Kubernetes job was created.
 	// +optional
 	CreationTime *meta_v1.Time `json:"creationTime,omitempty"`
 
+	// The time at which the Kubernetes job was started.
 	// +optional
 	StartTime *meta_v1.Time `json:"startTime,omitempty"`
 
+	// The time at which the batch job ended.
+	// The value is set when phase is either Succeeded, Failed or Stopped.
+	// - Succeeded: Value from CompletionTime of the Kubernetes job.
+	// - Failed: Value from LastTransitionTime of the Failed condition of the Kubernetes job.
+	// - Stopped: The timestamp a job with Stop=true was reonciled.
 	// +optional
 	EndTime *meta_v1.Time `json:"endTime,omitempty"`
 }
 
+// LocalObjectReference contains enough information to let you locate the
+// referenced object inside the same namespace.
 type LocalObjectReference struct {
-	// Name of the resource being referred to.
+	// Name of the referent.
 	// More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
 	// +kubebuilder:validation:MaxLength:=253
 	Name string `json:"name"`
