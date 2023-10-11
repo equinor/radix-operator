465b26317cd7e9abdac27a556dcd1af1900b2659
Nils Gustav Stråbø <nst@equinor.com>
2023-10-11 14:57:11 +0200

    function for polling RESTClient until success

--- pkg/apis/utils/init.go
+++ pkg/apis/utils/init.go
@@ -12,7 +12,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -38,6 +37,7 @@ func WithKubernetesClientRateLimiter(rateLimiter flowcontrol.RateLimiter) Kubern
 // GetKubernetesClient Gets clients to talk to the API
 func GetKubernetesClient(configOptions ...KubernetesClientConfigOption) (kubernetes.Interface, radixclient.Interface, monitoring.Interface, secretProviderClient.Interface) {
 	ctx := context.Background()
+	pollTimeout, pollInterval := time.Minute, 15*time.Second
 	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
 	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 
@@ -55,28 +55,28 @@ func GetKubernetesClient(configOptions ...KubernetesClientConfigOption) (kuberne
 		o(config)
 	}
 
-	client, err := waitForClientWithSuccessfulConnection(ctx, func() (*kubernetes.Clientset, error) {
+	client, err := PollRESTClientUntilSuccessfulConnection(ctx, pollTimeout, pollInterval, func() (*kubernetes.Clientset, error) {
 		return kubernetes.NewForConfig(config)
 	})
 	if err != nil {
 		log.Fatalf("getClusterConfig k8s client: %v", err)
 	}
 
-	radixClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*radixclient.Clientset, error) {
+	radixClient, err := PollRESTClientUntilSuccessfulConnection(ctx, pollTimeout, pollInterval, func() (*radixclient.Clientset, error) {
 		return radixclient.NewForConfig(config)
 	})
 	if err != nil {
 		log.Fatalf("getClusterConfig radix client: %v", err)
 	}
 
-	prometheusOperatorClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*monitoring.Clientset, error) {
+	prometheusOperatorClient, err := PollRESTClientUntilSuccessfulConnection(ctx, pollTimeout, pollInterval, func() (*monitoring.Clientset, error) {
 		return monitoring.NewForConfig(config)
 	})
 	if err != nil {
 		log.Fatalf("getClusterConfig prometheus-operator client: %v", err)
 	}
 
-	secretProviderClient, err := waitForClientWithSuccessfulConnection(ctx, func() (*secretProviderClient.Clientset, error) {
+	secretProviderClient, err := PollRESTClientUntilSuccessfulConnection(ctx, pollTimeout, pollInterval, func() (*secretProviderClient.Clientset, error) {
 		return secretProviderClient.NewForConfig(config)
 	})
 	if err != nil {
@@ -85,31 +85,4 @@ func GetKubernetesClient(configOptions ...KubernetesClientConfigOption) (kuberne
 
 	log.Printf("Successfully constructed k8s client to API server %v", config.Host)
 	return client, radixClient, prometheusOperatorClient, secretProviderClient
-}
-
-func waitForClientWithSuccessfulConnection[T interface{ RESTClient() rest.Interface }](ctx context.Context, clientFactory func() (T, error)) (T, error) {
-	var client T
-	timeoutCtx, cancel := context.WithTimeout(ctx, 60*time.Second)
-	defer cancel()
-
-	err := wait.PollUntilContextCancel(timeoutCtx, 15*time.Second, true, func(ctx context.Context) (done bool, err error) {
-		c, err := clientFactory()
-		if err != nil {
-			return false, err
-		}
-
-		// Retry if error transient, e.g. TLS handshake timeout
-		if err := c.RESTClient().Get().Do(timeoutCtx).Error(); err != nil && isTransientConnectionError(err) {
-			log.Infof("transient error when connecting, retrying: %v", err)
-			return false, nil
-		}
-		client = c
-		return true, nil
-	})
-	return client, err
-}
-
-func isTransientConnectionError(err error) bool {
-	// TODO: Should we check for other connection errors that are transient, e.g. net.DNSError?
-	return err.Error() == "net/http: TLS handshake timeout"
 }
