76b1806746658be3d1e516e6713a8fac64179957
Sergey Smolnikov <ssmol@equinor.com>
2024-01-03 10:17:11 +0100

    Fixed reported issues

--- pkg/apis/applicationconfig/environment.go
+++ pkg/apis/applicationconfig/environment.go
@@ -30,28 +30,20 @@ func (app *ApplicationConfig) buildRadixEnvironment(env radixv1.Environment) *ra
 		WithEnvironmentName(env.Name).
 		WithRegistrationOwner(app.registration).
 		WithEgressConfig(env.Egress).
-		// Orphaned flag will be set by the environment handler but until
-		// reconciliation we must ensure it is false
-		// Update: It seems Update method does not update status object when using real k8s client, but the fake client does.
-		// Only an explicit call to UpdateStatus can update status object, and this is only done by the RadixEnvironment controller.
-		WithOrphaned(false).
 		BuildRE()
 }
 
 // syncEnvironment creates an environment or applies changes if it exists
-func (app *ApplicationConfig) syncEnvironment(newRE *radixv1.RadixEnvironment) error {
-	name := newRE.GetName()
-	logger := log.WithFields(log.Fields{"environment": name})
-	logger.Debugf("Apply RadixEnvironment ")
-	oldRE, err := app.getRadixEnvironment(name)
-	if err != nil {
+func (app *ApplicationConfig) syncEnvironment(radixEnvironment *radixv1.RadixEnvironment) error {
+	logger := log.WithFields(log.Fields{"environment": radixEnvironment.GetName()})
+	logger.Debugf("Apply RadixEnvironment")
+	if _, err := app.getRadixEnvironment(radixEnvironment.GetName()); err != nil {
 		if errors.IsNotFound(err) {
-			return app.createRadixEnvironment(newRE, logger)
+			return app.createRadixEnvironment(radixEnvironment, logger)
 		}
 		return fmt.Errorf("failed to get RadixEnvironment: %v", err)
 	}
-	logger.Debugf("taken RadixEnvironment (revision %s)", oldRE.GetResourceVersion())
-	return app.updateRadixEnvironment(oldRE, newRE, logger)
+	return app.updateRadixEnvironment(radixEnvironment, logger)
 }
 
 func (app *ApplicationConfig) createRadixEnvironment(radixEnvironment *radixv1.RadixEnvironment, logger *log.Entry) error {
@@ -63,36 +55,27 @@ func (app *ApplicationConfig) createRadixEnvironment(radixEnvironment *radixv1.R
 	return nil
 }
 
-// updateRadixEnvironment creates a mergepatch, comparing old and new RadixEnvironments and issues the patch to radix
-func (app *ApplicationConfig) updateRadixEnvironment(oldRE *radixv1.RadixEnvironment, newRE *radixv1.RadixEnvironment, logger *log.Entry) error {
-	radixEnvironment := oldRE.DeepCopy()
-	logger.Debugf("update RadixEnvironment (revision %s)", oldRE.GetResourceVersion())
-	name := newRE.GetName()
+// updateRadixEnvironment updates a RadixEnvironment
+func (app *ApplicationConfig) updateRadixEnvironment(radixEnvironment *radixv1.RadixEnvironment, logger *log.Entry) error {
 	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		radixEnvironment.Spec = newRE.Spec
-
-		// Will perform update as patching does not seem to work for this custom resource
-		updated, err := app.kubeutil.UpdateRadixEnvironment(radixEnvironment)
-		if err == nil {
-			logger.Debugf("Updated RadixEnvironment (revision %s)", updated.GetResourceVersion())
-			return nil
-		}
-		if errors.IsNotFound(err) {
-			logger.Debugf("RadixEnvironment does not exist")
-			return nil
-		}
-		if !errors.IsConflict(err) {
-			return fmt.Errorf("failed to update RadixEnvironment object: %v", err)
-		}
-		if radixEnvironment, err = app.getRadixEnvironment(name); err != nil {
+		existingRE, err := app.getRadixEnvironment(radixEnvironment.GetName())
+		if err != nil {
 			if errors.IsNotFound(err) {
-				logger.Debugf("RadixEnvironment does not exist")
 				return nil
 			}
-			logger.Debugf("re-taken RadixEnvironment  revision %s", radixEnvironment.GetResourceVersion())
 			return err
 		}
-		return stderrors.New("failed to update changed RadixEnvironment")
+		logger.Debugf("re-taken RadixEnvironment (revision %s)", existingRE.GetResourceVersion())
+
+		newRE := existingRE.DeepCopy()
+		newRE.Spec = radixEnvironment.Spec
+		// Will perform update as patching does not seem to work for this custom resource
+		updated, err := app.kubeutil.UpdateRadixEnvironment(newRE)
+		if err != nil {
+			return fmt.Errorf("failed to update RadixEnvironment: %v", err)
+		}
+		logger.Debugf("Updated RadixEnvironment (revision %s)", updated.GetResourceVersion())
+		return nil
 	})
 }
 
