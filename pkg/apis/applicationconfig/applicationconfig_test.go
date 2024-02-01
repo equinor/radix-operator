75bcbbde07b452d80358a3c9394c3dbf6ee5af2d
Richard Hagen <richard.hagen@bouvet.no>
2024-02-01 13:06:55 +0100

    add relicator annotation to private-image-hubs secret

--- pkg/apis/applicationconfig/applicationconfig_test.go
+++ pkg/apis/applicationconfig/applicationconfig_test.go
@@ -461,6 +461,7 @@ func Test_WithPrivateImageHubSet_SecretsCorrectly_Added(t *testing.T) {
 		"{\"auths\":{\"privaterepodeleteme.azurecr.io\":{\"username\":\"814607e6-3d71-44a7-8476-50e8b281abbc\",\"password\":\"\",\"email\":\"radix@equinor.com\",\"auth\":\"ODE0NjA3ZTYtM2Q3MS00NGE3LTg0NzYtNTBlOGIyODFhYmJjOg==\"}}}",
 		string(secret.Data[corev1.DockerConfigJsonKey]))
 	assert.Equal(t, "radix-private-image-hubs-sync=any-app", secret.ObjectMeta.Annotations["kubed.appscode.com/sync"])
+	assert.Equal(t, "radix-private-image-hubs-sync=any-app", secret.ObjectMeta.Annotations["replicator.v1.mittwald.de/replicate-to-matching"])
 }
 
 func Test_WithPrivateImageHubSet_SecretsCorrectly_UpdatedNewAdded(t *testing.T) {
