0d8fc7b348874c01d3fb6beac37f618df4a21e9c
Sergey Smolnikov <ssmol@equinor.com>
2024-03-22 09:57:47 +0100

    Update pipeline-runner/steps/deploy.go
    Co-authored-by: Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>

--- pipeline-runner/steps/deploy.go
+++ pipeline-runner/steps/deploy.go
@@ -128,7 +128,7 @@ func (cli *DeployStepImplementation) deployToEnv(appName, envName string, pipeli
 		log.Error().Err(err).Msgf("Failed while waiting for the Radix deployment %s on environment %s gets active. Delete this Radix deployment.", radixDeploymentName, envName)
 		if err := cli.GetRadixclient().RadixV1().RadixDeployments(radixDeployment.GetNamespace()).Delete(context.Background(), radixDeploymentName, metav1.DeleteOptions{}); err != nil {
 			// && !k8errs.IsNotFound(err) && !k8errs.IsForbidden(err) {
-			log.Error().Err(err).Msgf("Failed to delete the Radix deployment.")
+			log.Error().Err(err).Msgf("Failed to delete Radix deployment")
 		}
 		return err
 	}
