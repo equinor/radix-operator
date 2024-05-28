239285051e93ac6615dc44af0dad3a6ce3c1c833
Richard87 <richard.hagen@gmail.com>
2024-05-28 09:52:48 +0200

    remove unused interface

--- pkg/apis/utils/deploymentcomponent_builder.go
+++ pkg/apis/utils/deploymentcomponent_builder.go
@@ -4,10 +4,6 @@ import (
 	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 )
 
-type HorizontalScalingBuilder interface {
-	Build() *v1.RadixHorizontalScaling
-}
-
 // DeployComponentBuilder Handles construction of RD component
 type DeployComponentBuilder interface {
 	WithName(string) DeployComponentBuilder
