1b7aeb389a29772c6f04c65168347a1ce3e22a4e
Nils Gustav Stråbø <nst@equinor.com>
2024-06-04 15:53:38 +0200

    remove unused code

--- pkg/apis/pipeline/container_output_name.go
+++
@@ -1,6 +0,0 @@
-package pipeline
-
-// ContainerOutputName holds information about the configmap a container writes output to.
-// Key is the name of the container and value is the name of the configmap.
-// The configmap must exist in the same namespace as the container Pod.
-type ContainerOutputName map[string]string
