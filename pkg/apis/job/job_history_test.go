8c3baf8b5dc1843c1c9207449979370a9900a396
Sergey Smolnikov <ssmol@equinor.com>
2024-07-31 14:15:11 +0200

    Added tests

--- pkg/apis/job/job_history_test.go
+++ pkg/apis/job/job_history_test.go
@@ -19,18 +19,13 @@ import (
 	secretproviderfake "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned/fake"
 )
 
-type RadixJobHistoryTestSuiteBase struct {
+type RadixJobHistoryTestSuite struct {
 	suite.Suite
 	kubeUtils   *kube.Kube
 	radixClient radixclient.Interface
 }
 
-func (s *RadixJobHistoryTestSuiteBase) SetupTest() {
-	s.setupTest()
-}
-
-func (s *RadixJobHistoryTestSuiteBase) setupTest() {
-	// Setup
+func (s *RadixJobHistoryTestSuite) setupTest() {
 	kubeClient := kubernetes.NewSimpleClientset()
 	radixClient := radix.NewSimpleClientset()
 	kedaClient := kedafake.NewSimpleClientset()
@@ -39,38 +34,70 @@ func (s *RadixJobHistoryTestSuiteBase) setupTest() {
 	s.kubeUtils, s.radixClient = kubeUtil, radixClient
 }
 
-func TestRadixJobTestSuite(t *testing.T) {
+func TestRadixJobHistoryTestSuite(t *testing.T) {
 	suite.Run(t, new(RadixJobHistoryTestSuite))
 }
 
-type RadixJobHistoryTestSuite struct {
-	RadixJobHistoryTestSuiteBase
-}
-
-func (s *RadixJobHistoryTestSuite) TestObjectSynced_StatusMissing_StatusFromAnnotation() {
+func (s *RadixJobHistoryTestSuite) TestObjectSynced_Deleted() {
 	const (
-		appName1 = "anyApp"
-		jobName1 = "anyJob1"
-		jobName2 = "anyJob2"
-		jobName3 = "anyJob3"
+		appName1 = "any-app1"
+		appName2 = "any-app2"
+		jobName1 = "any-job1"
+		jobName2 = "any-job2"
+		jobName3 = "any-job3"
 	)
-	s.createRadixJob(appName1, jobName1)
+	type radixJobsMap map[string]struct{}
+	type appRadixJobsMap map[string]radixJobsMap
+	type scenario struct {
+		name              string
+		historyLimit      int
+		initTest          func()
+		expectedRadixJobs appRadixJobsMap
+	}
 
-	namespace := utils.GetAppNamespace(appName1)
-	radixJobList, err := s.radixClient.RadixV1().RadixJobs(namespace).List(context.Background(), metav1.ListOptions{})
-	s.NoError(err)
-	s.Require().Len(radixJobList.Items, 1)
+	scenarios := []scenario{
+		{
+			name:         "No jobs deleted",
+			historyLimit: 1,
+			initTest: func() {
+				s.createRadixJob(appName1, jobName1)
+			},
+			expectedRadixJobs: appRadixJobsMap{appName1: radixJobsMap{jobName1: struct{}{}}},
+		},
+	}
 
-	done := make(chan struct{})
-	job.NewHistory(s.radixClient, s.kubeUtils, 1, job.WithDoneChannel(done)).
-		Cleanup(context.Background(), appName1, jobName1)
-	select {
-	case <-done:
-		radixJobList, err := s.radixClient.RadixV1().RadixJobs(namespace).List(context.Background(), metav1.ListOptions{})
-		s.NoError(err)
-		s.Require().Len(radixJobList.Items, 1)
-	case <-time.After(10 * time.Second):
-		s.Fail("Timed out")
+	for _, ts := range scenarios {
+		s.T().Run(ts.name, func(t *testing.T) {
+			s.setupTest()
+			ts.initTest()
+			s.T().Run("Cleanup", func(t *testing.T) {
+
+				done := make(chan struct{})
+				job.NewHistory(s.radixClient, s.kubeUtils, ts.historyLimit, job.WithDoneChannel(done)).
+					Cleanup(context.Background(), appName1, jobName1)
+
+				expectedJobCount := 0
+				for _, jobsMap := range ts.expectedRadixJobs {
+					expectedJobCount += len(jobsMap)
+				}
+				select {
+				case <-done:
+					actualRadixJobList, err := s.radixClient.RadixV1().RadixJobs(utils.GetAppNamespace(appName1)).List(context.Background(), metav1.ListOptions{})
+					s.NoError(err)
+					s.Len(actualRadixJobList.Items, expectedJobCount, "RadixJob count")
+					for _, radixJob := range actualRadixJobList.Items {
+						jobsMap, ok := ts.expectedRadixJobs[radixJob.Spec.AppName]
+						s.True(ok, "missing RadixJobs for the app %s", radixJob.Spec.AppName)
+						_, ok = jobsMap[radixJob.Name]
+						s.True(ok, "missing RadixJob %s for the app %s", radixJob.Name, radixJob.Spec.AppName)
+					}
+					s.Equal(jobName1, actualRadixJobList.Items[0].Name)
+				case <-time.After(10 * time.Second):
+					s.Fail("Timed out")
+				}
+			})
+		})
+
 	}
 }
 
@@ -83,13 +110,16 @@ func (s *RadixJobHistoryTestSuite) createRadixJob(appName string, jobName1 strin
 }
 
 func createRadixJob(appName, jobName string) *radixv1.RadixJob {
-	return &radixv1.RadixJob{ObjectMeta: metav1.ObjectMeta{
-		Name: jobName,
-		Labels: radixlabels.Merge(
-			radixlabels.ForApplicationName(appName),
-			radixlabels.ForPipelineJobName(jobName),
-			radixlabels.ForPipelineJobType(),
-			radixlabels.ForPipelineJobPipelineType(radixv1.BuildDeploy),
-		),
-	}}
+	return &radixv1.RadixJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: jobName,
+			Labels: radixlabels.Merge(
+				radixlabels.ForApplicationName(appName),
+				radixlabels.ForPipelineJobName(jobName),
+				radixlabels.ForPipelineJobType(),
+				radixlabels.ForPipelineJobPipelineType(radixv1.BuildDeploy),
+			),
+		},
+		Spec: radixv1.RadixJobSpec{AppName: appName},
+	}
 }
