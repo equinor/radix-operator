962bc175b3b76fa8ef15740e8cc520f96813e539
Richard87 <richard.hagen@gmail.com>
2024-09-24 14:17:24 +0200

    Revert validation of AdGroups test

--- pkg/apis/radixvalidators/validate_rr_test.go
+++ pkg/apis/radixvalidators/validate_rr_test.go
@@ -56,6 +56,7 @@ func TestCanRadixApplicationBeInserted(t *testing.T) {
 		{"empty ad group", func(rr *v1.RadixRegistration) { rr.Spec.AdGroups = []string{""} }, nil},
 		{"empty configBranch", func(rr *v1.RadixRegistration) { rr.Spec.ConfigBranch = "" }, nil},
 		{"invalid configBranch", func(rr *v1.RadixRegistration) { rr.Spec.ConfigBranch = "main.." }, nil},
+		{"empty ad groups", func(rr *v1.RadixRegistration) { rr.Spec.AdGroups = nil }, []radixvalidators.RadixRegistrationValidator{radixvalidators.RequireAdGroups}},
 	}
 
 	_, client := validRRSetup()
