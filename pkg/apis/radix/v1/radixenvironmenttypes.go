555598c41cc83df0bebfa67f890394ed5519c473
Sergey Smolnikov <ssmol@equinor.com>
2024-10-11 10:37:18 +0200

    Fixed issues

--- pkg/apis/radix/v1/radixenvironmenttypes.go
+++ pkg/apis/radix/v1/radixenvironmenttypes.go
@@ -10,32 +10,32 @@ import (
 
 // RadixEnvironment is a Custom Resource Definition
 type RadixEnvironment struct {
-	metav1.TypeMeta   `json:",inline" yaml:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty" yaml:"metadata,omitempty"`
-	Spec              RadixEnvironmentSpec   `json:"spec" yaml:"spec"`
-	Status            RadixEnvironmentStatus `json:"status" yaml:"status"`
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              RadixEnvironmentSpec   `json:"spec"`
+	Status            RadixEnvironmentStatus `json:"status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // RadixEnvironmentList is a list of REs
 type RadixEnvironmentList struct {
-	metav1.TypeMeta `json:",inline" yaml:",inline"`
-	metav1.ListMeta `json:"metadata" yaml:"metadata"`
-	Items           []RadixEnvironment `json:"items" yaml:"items"`
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []RadixEnvironment `json:"items"`
 }
 
 // RadixEnvironmentSpec is the spec for an RE
 type RadixEnvironmentSpec struct {
-	AppName string       `json:"appName" yaml:"appName"`
-	EnvName string       `json:"envName" yaml:"envName"`
-	Egress  EgressConfig `json:"egress,omitempty" yaml:"egress,omitempty"`
+	AppName string       `json:"appName"`
+	EnvName string       `json:"envName"`
+	Egress  EgressConfig `json:"egress,omitempty"`
 }
 
 // RadixEnvironmentStatus is the status for an RE
 type RadixEnvironmentStatus struct {
-	Reconciled metav1.Time `json:"reconciled" yaml:"reconciled"`
-	Orphaned   bool        `json:"orphaned" yaml:"orphaned"`
+	Reconciled metav1.Time `json:"reconciled"`
+	Orphaned   bool        `json:"orphaned"`
 	// OrphanedTimestamp is a timestamp representing the server time when this RadixEnvironment was removed from the RadixApplication
-	OrphanedTimestamp *metav1.Time `json:"orphanedTimestamp" yaml:"orphanedTimestamp"`
+	OrphanedTimestamp *metav1.Time `json:"orphanedTimestamp,omitempty"`
 }
