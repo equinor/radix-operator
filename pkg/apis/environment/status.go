cc1a6c1c3a8c587b8bab8eae79ce593f63ec80fb
Sergey Smolnikov <ssmol@equinor.com>
2024-10-14 10:34:42 +0200

    Fixed sync radix environment error

--- pkg/apis/environment/status.go
+++ pkg/apis/environment/status.go
@@ -12,19 +12,13 @@ import (
 )
 
 func (env *Environment) syncStatus(ctx context.Context, radixEnvironment *radixv1.RadixEnvironment, time metav1.Time) error {
-	if err := updateRadixEnvironmentStatus(ctx, env.radixclient, env.appConfig, radixEnvironment, time); err != nil {
+	if err := env.updateRadixEnvironmentStatus(ctx, env.radixclient, env.appConfig, radixEnvironment, time); err != nil {
 		return fmt.Errorf("failed to update status on environment %s: %w", radixEnvironment.Spec.EnvName, err)
 	}
-	currentEnv, err := env.radixclient.RadixV1().RadixEnvironments().Get(ctx, radixEnvironment.Spec.EnvName, metav1.GetOptions{})
-	if err != nil {
-		return err
-	}
-	env.config = currentEnv
-	env.logger.Debug().Msgf("updated status of RadixEnvironment (revision %s)", currentEnv.GetResourceVersion())
 	return nil
 }
 
-func updateRadixEnvironmentStatus(ctx context.Context, radixClient versioned.Interface, radixApplication *radixv1.RadixApplication, radixEnvironment *radixv1.RadixEnvironment, time metav1.Time) error {
+func (env *Environment) updateRadixEnvironmentStatus(ctx context.Context, radixClient versioned.Interface, radixApplication *radixv1.RadixApplication, radixEnvironment *radixv1.RadixEnvironment, time metav1.Time) error {
 	radixEnvironmentInterface := radixClient.RadixV1().RadixEnvironments()
 	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		name := radixEnvironment.GetName()
@@ -33,7 +27,16 @@ func updateRadixEnvironmentStatus(ctx context.Context, radixClient versioned.Int
 			return err
 		}
 		changeStatus(radixApplication, radixEnvironment.Spec.EnvName, &currentEnv.Status, time)
-		_, err = radixEnvironmentInterface.UpdateStatus(ctx, currentEnv, metav1.UpdateOptions{})
+		updated, err := radixEnvironmentInterface.UpdateStatus(ctx, currentEnv, metav1.UpdateOptions{})
+		if err == nil && env.config.GetName() == name {
+			currentEnv, err = radixEnvironmentInterface.Get(ctx, name, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			env.config = currentEnv
+			env.logger.Debug().Msgf("updated status of RadixEnvironment (revision %s)", updated.GetResourceVersion())
+			return nil
+		}
 		return err
 	})
 }
