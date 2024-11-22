8e16e5092371df549e97234874d48ee3fe72e5da
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2024-11-22 08:26:37 +0100

    Fix restart batchjob infinite loop (#1228)

--- pkg/apis/batch/utils.go
+++ pkg/apis/batch/utils.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 
 	"github.com/equinor/radix-common/utils"
-	"github.com/equinor/radix-common/utils/slice"
 	radixv1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 	radixlabels "github.com/equinor/radix-operator/pkg/apis/utils/labels"
+	batchv1 "k8s.io/api/batch/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubelabels "k8s.io/apimachinery/pkg/labels"
 )
@@ -31,34 +31,10 @@ func isJobStatusWaiting(jobStatus radixv1.RadixBatchJobStatus) bool {
 	return jobStatus.Phase == radixv1.BatchJobPhaseWaiting
 }
 
-func isBatchDone(batch *radixv1.RadixBatch) bool {
-	jobStatusesMap := slice.Reduce(batch.Status.JobStatuses, make(map[string]radixv1.RadixBatchJobStatus), func(acc map[string]radixv1.RadixBatchJobStatus, jobStatus radixv1.RadixBatchJobStatus) map[string]radixv1.RadixBatchJobStatus {
-		acc[jobStatus.Name] = jobStatus
-		return acc
-	})
-	for _, batchJob := range batch.Spec.Jobs {
-		jobStatus, ok := jobStatusesMap[batchJob.Name]
-		if !ok {
-			return false
-		}
-		if !isJobStatusDone(jobStatus) || needRestartJob(batchJob.Restart, jobStatus.Restart) {
-			return false
-		}
-	}
-	return batch.Status.Condition.Type == radixv1.BatchConditionTypeCompleted
-}
-
 func isJobStatusDone(jobStatus radixv1.RadixBatchJobStatus) bool {
 	return jobStatus.Phase == radixv1.BatchJobPhaseSucceeded ||
 		jobStatus.Phase == radixv1.BatchJobPhaseFailed ||
 		jobStatus.Phase == radixv1.BatchJobPhaseStopped
-}
-
-func isBatchJobDone(batch *radixv1.RadixBatch, batchJobName string) bool {
-	return slice.Any(batch.Status.JobStatuses,
-		func(jobStatus radixv1.RadixBatchJobStatus) bool {
-			return jobStatus.Name == batchJobName && isJobStatusDone(jobStatus)
-		})
 }
 
 func ownerReference(job *radixv1.RadixBatch) []metav1.OwnerReference {
@@ -70,5 +46,11 @@ func ownerReference(job *radixv1.RadixBatch) []metav1.OwnerReference {
 			UID:        job.UID,
 			Controller: utils.BoolPtr(true),
 		},
+	}
+}
+
+func isKubeJobForBatchJob(batchJob *radixv1.RadixBatchJob) func(job *batchv1.Job) bool {
+	return func(job *batchv1.Job) bool {
+		return isResourceLabeledWithBatchJobName(batchJob.Name, job)
 	}
 }
