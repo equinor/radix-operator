a4898a65cce60460eb51e897a1cc233cda118f66
Sergey Smolnikov <ssmol@equinor.com>
2025-02-20 16:16:21 +0100

    Revert first letter of the Credentials enum (#1296)
    * Revert first letter of the Credentials enum
    
    * Added small first letter of the Credentials enum
    
    * Removed capital first letter from the Credentials enum

--- pkg/apis/radix/v1/radixapptypes.go
+++ pkg/apis/radix/v1/radixapptypes.go
@@ -1390,8 +1390,8 @@ type OAuth2 struct {
 	RedisStore *OAuth2RedisStore `json:"redisStore,omitempty"`
 
 	// Credentials defines credentials type for authenticating. Default is a Secret, which represents a client secret.
-	// +kubebuilder:validation:Enum=Secret;AzureWorkloadIdentity
-	// +kubebuilder:default:=Secret
+	// +kubebuilder:validation:Enum=secret;azureWorkloadIdentity;""
+	// +kubebuilder:default:=secret
 	// +optional
 	Credentials CredentialsType `json:"credentials,omitempty"`
 }
