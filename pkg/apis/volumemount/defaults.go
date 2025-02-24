5296fecd1dfa8b8acd0cb01c7f85050bc148d2bf
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-02-24 10:35:31 +0100

    blobfuse2: set max memory cache for streaming (#1297)

--- pkg/apis/volumemount/defaults.go
+++ pkg/apis/volumemount/defaults.go
@@ -11,15 +11,9 @@ const (
 	csiPersistentVolumeClaimNameTemplate = "pvc-%s-%s"              // pvc-<volumename>-<randomstring5>
 	csiPersistentVolumeNameTemplate      = "pv-radixvolumemount-%s" // pv-<guid>
 
-	csiMountOptionGid                       = "gid"                 // Volume mount owner GroupID. Used when drivers do not honor fsGroup securityContext setting
-	csiMountOptionUid                       = "uid"                 // Volume mount owner UserID. Used instead of GroupID
-	csiMountOptionUseAdls                   = "use-adls"            // Use ADLS or Block Blob
-	csiMountOptionStreamingEnabled          = "streaming"           // Enable Streaming
-	csiMountOptionStreamingCache            = "stream-cache-mb"     // Limit total amount of data being cached in memory to conserve memory
-	csiMountOptionStreamingMaxBlocksPerFile = "max-blocks-per-file" // Maximum number of blocks to be cached in memory for streaming
-	csiMountOptionStreamingMaxBuffers       = "max-buffers"         // The total number of buffers to be cached in memory (in MB).
-	csiMountOptionStreamingBlockSize        = "block-size-mb"       // The size of each block to be cached in memory (in MB).
-	csiMountOptionStreamingBufferSize       = "buffer-size-mb"      // The size of each buffer to be cached in memory (in MB).
+	csiMountOptionGid     = "gid"      // Volume mount owner GroupID. Used when drivers do not honor fsGroup securityContext setting
+	csiMountOptionUid     = "uid"      // Volume mount owner UserID. Used instead of GroupID
+	csiMountOptionUseAdls = "use-adls" // Use ADLS or Block Blob
 
 	csiVolumeAttributeProtocolParameterFuse  = "fuse"  // Protocol "blobfuse"
 	csiVolumeAttributeProtocolParameterFuse2 = "fuse2" // Protocol "blobfuse2"
