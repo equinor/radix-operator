d61120a00dcb2edfca8995bbbc77ae5fb4786464
Sergey Smolnikov <ssmol@equinor.com>
2025-03-28 16:30:34 +0100

    Cleanup

--- pkg/apis/defaults/configmaps.go
+++ pkg/apis/defaults/configmaps.go
@@ -1,14 +1,6 @@
 package defaults
 
 const (
-	// RadixGitCommitHashKey GitHub commit hash for pipeline build source
-	RadixGitCommitHashKey = "git-commit-hash"
-	// RadixGitTagsKey GitHub tags for pipeline build source
-	RadixGitTagsKey = "git-tags"
-	// RadixGitChangedFolders List of changed files folders in the GitHub source
-	RadixGitChangedFolders = "git-changed-folders"
-	// RadixGitChangedChangedRadixConfig Indicates if Radix config file is changed in the GitHub source
-	RadixGitChangedChangedRadixConfig = "git-changed-radixconfig"
-	GitPublicKeyConfigMapName         = "git-ssh-public-key"
-	GitPublicKeyConfigMapKey          = "public_key"
+	GitPublicKeyConfigMapName = "git-ssh-public-key"
+	GitPublicKeyConfigMapKey  = "public_key"
 )
