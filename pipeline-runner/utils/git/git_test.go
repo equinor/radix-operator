3fed8abcc58ef9423a5b8fead7b0394095fdc81e
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-05-23 14:10:15 +0200

    git unit tests

--- pipeline-runner/utils/git/git_test.go
+++ pipeline-runner/utils/git/git_test.go
@@ -105,160 +105,73 @@ func tearDownGitTest() {
 
 func Test_DiffCommits(t *testing.T) {
 	scenarios := map[string]struct {
-		beforeCommitExclusive  string
-		targetCommit           string
-		expectedChangedFolders []string
-		expectedError          error
+		beforeCommit  string
+		targetCommit  string
+		expectedDiffs git.DiffEntries
+		expectedError error
 	}{
-		"init - add radixconfig and gitignore files": {
-			targetCommit:           "7d6309f7537baa2815bb631802e6d8d613150c52",
-			beforeCommitExclusive:  "",
-			expectedChangedFolders: []string{"."},
+		"different commits": {
+			targetCommit: "157014b59d6b24205b4fbf57165f0029c49d7963",
+			beforeCommit: "0b9ee1f93639fff492c05b8d5e662301f508debe",
+			expectedDiffs: git.DiffEntries{
+				{Name: "app1/Dockerfile"},
+				{Name: "app1/app.js"},
+				{Name: "app2/Dockerfile"},
+				{Name: "app2/app.js"},
+			},
 		},
-		"init - add radixconfig and gitignore files with same commit": {
-			targetCommit:           "7d6309f7537baa2815bb631802e6d8d613150c52",
-			beforeCommitExclusive:  "7d6309f7537baa2815bb631802e6d8d613150c52",
-			expectedChangedFolders: nil,
+		"different commits in reverse order should give same result": {
+			targetCommit: "0b9ee1f93639fff492c05b8d5e662301f508debe",
+			beforeCommit: "157014b59d6b24205b4fbf57165f0029c49d7963",
+			expectedDiffs: git.DiffEntries{
+				{Name: "app1/Dockerfile"},
+				{Name: "app1/app.js"},
+				{Name: "app2/Dockerfile"},
+				{Name: "app2/app.js"},
+			},
 		},
-		"added app1 folder and its files. app1 component added to the radixconfig": {
-			targetCommit:           "0b9ee1f93639fff492c05b8d5e662301f508debe",
-			beforeCommitExclusive:  "7d6309f7537baa2815bb631802e6d8d613150c52",
-			expectedChangedFolders: []string{".", "app1"},
+		"different commits in different branches (before: dev, after: dev)": {
+			targetCommit: "cb65b06a331b4e363f32c88a94d2fd145e38b2cf",
+			beforeCommit: "dd81c903e402b4120cbf2431393ced321b243b45",
+			expectedDiffs: git.DiffEntries{
+				{Name: ".gitattributes"},
+				{Name: ".gitignore"},
+				{Name: "app1/app.js"},
+				{Name: "app1/data/index.md"},
+				{Name: "app1/data/level2/data.json"},
+				{Name: "app1/data/level2/test-data-git-commits-blobless.zip"},
+				{Name: "app1/data/level2/test_data.zip"},
+				{Name: "app3/app.js"},
+				{Name: "app3/data/index.md"},
+				{Name: "app3/radixconfig.yaml"},
+				{Name: "app4/app.js"},
+				{Name: "radixconfig.yaml"},
+			},
 		},
-		"Changed radixconfig, but config branch is different": {
-			targetCommit:           "0b9ee1f93639fff492c05b8d5e662301f508debe",
-			beforeCommitExclusive:  "7d6309f7537baa2815bb631802e6d8d613150c52",
-			expectedChangedFolders: []string{".", "app1"},
+		"same commit should give empty list": {
+			targetCommit:  "fe148016c5fcd68e3a3b42d32f1c02586072defc",
+			beforeCommit:  "fe148016c5fcd68e3a3b42d32f1c02586072defc",
+			expectedDiffs: git.DiffEntries{},
 		},
-		"changed files in the folder app1": {
-			targetCommit:           "f68e88664ed51f79880b7f69d5789d21086ed1dc",
-			beforeCommitExclusive:  "0b9ee1f93639fff492c05b8d5e662301f508debe",
-			expectedChangedFolders: []string{"app1"},
-		},
-		"the same target and before commit, files were changed": {
-			targetCommit:           "f68e88664ed51f79880b7f69d5789d21086ed1dc",
-			beforeCommitExclusive:  "f68e88664ed51f79880b7f69d5789d21086ed1dc",
-			expectedChangedFolders: nil,
-		},
-		"the same target and before commit, only radixconfig was changed": {
-			targetCommit:           "38845f7ba0b9dbfa0a0a929aaddc7308f4db35e2",
-			beforeCommitExclusive:  "38845f7ba0b9dbfa0a0a929aaddc7308f4db35e2",
-			expectedChangedFolders: nil,
+		"empty before commit includes changes in initial commit": {
+			targetCommit: "936011be645e11a1a8773ca61686176dbf62ba81",
+			beforeCommit: "",
+			expectedDiffs: git.DiffEntries{
+				{Name: "app1/Dockerfile"},
+				{Name: "app1/app.js"},
+				{Name: "radixconfig.yaml"},
+				{Name: ".gitignore"},
+			},
 		},
 		"invalid target commit": {
-			targetCommit:          "invalid-commit",
-			beforeCommitExclusive: "",
-			expectedError:         git.ErrCommitNotFound,
+			targetCommit:  "invalid-value",
+			beforeCommit:  "fe148016c5fcd68e3a3b42d32f1c02586072defc",
+			expectedError: git.ErrCommitNotFound,
 		},
-		"invalid empty target commit": {
-			targetCommit:          "",
-			beforeCommitExclusive: "",
-			expectedError:         git.ErrEmptyCommitHash,
-		},
-		"Added folder app2 with files": {
-			targetCommit:           "157014b59d6b24205b4fbf57165f0029c49d7963",
-			beforeCommitExclusive:  "f68e88664ed51f79880b7f69d5789d21086ed1dc",
-			expectedChangedFolders: []string{"app2"},
-		},
-		"Folder app2 was renamed to app3, file in folder 3 was changed": {
-			targetCommit:           "31472b8fc3fe22a2b8d174e79ae2f891a975864d",
-			beforeCommitExclusive:  "157014b59d6b24205b4fbf57165f0029c49d7963",
-			expectedChangedFolders: []string{"app2", "app3"},
-		},
-		"File in folder 3 was changed": {
-			targetCommit:           "31472b8fc3fe22a2b8d174e79ae2f891a975864d",
-			beforeCommitExclusive:  "8d99f5318a7164ff1e03bd4fbe1cba554e9c906b",
-			expectedChangedFolders: []string{"app3"},
-		},
-		"radixconfig.yaml was changed": {
-			targetCommit:           "38845f7ba0b9dbfa0a0a929aaddc7308f4db35e2",
-			beforeCommitExclusive:  "2127927fa21ae471baefbadd3f05b60a4bf38b5f",
-			expectedChangedFolders: []string{"."},
-		},
-		"File in folder 3 was changed, same commit": {
-			targetCommit:           "31472b8fc3fe22a2b8d174e79ae2f891a975864d",
-			beforeCommitExclusive:  "31472b8fc3fe22a2b8d174e79ae2f891a975864d",
-			expectedChangedFolders: nil,
-		},
-		"radixconfig.yaml was added to the folder app3": {
-			targetCommit:           "d79cc7f32f58e30b01671b8bcc19f41508db95c8",
-			beforeCommitExclusive:  "31472b8fc3fe22a2b8d174e79ae2f891a975864d",
-			expectedChangedFolders: []string{"app3"},
-		},
-		"radixconfig.yaml was added to the folder app3, it is current config": {
-			targetCommit:           "d79cc7f32f58e30b01671b8bcc19f41508db95c8",
-			beforeCommitExclusive:  "31472b8fc3fe22a2b8d174e79ae2f891a975864d",
-			expectedChangedFolders: []string{"app3"},
-		},
-		"radixconfig.yaml was changed to the folder app3": {
-			targetCommit:           "0143afb54d8f9e5451b2dc34f47d7f933080e7a4",
-			beforeCommitExclusive:  "d79cc7f32f58e30b01671b8bcc19f41508db95c8",
-			expectedChangedFolders: []string{"app3"},
-		},
-		"radixconfig.yaml was changed to the folder app3, it is current config": {
-			targetCommit:           "0143afb54d8f9e5451b2dc34f47d7f933080e7a4",
-			beforeCommitExclusive:  "d79cc7f32f58e30b01671b8bcc19f41508db95c8",
-			expectedChangedFolders: []string{"app3"},
-		},
-		"Sub-folders were added to the folder app3, with file. Sub-folder was added to the folder app1, without file.": {
-			targetCommit:           "13bd8316267d6a0be44f3f87fe49e807e7bc24b4",
-			beforeCommitExclusive:  "0143afb54d8f9e5451b2dc34f47d7f933080e7a4",
-			expectedChangedFolders: []string{"app3/data/level2"},
-		},
-		"Files added and changed in the sub-folders of app3. File was added to the sub-folder in the folder app1.": {
-			targetCommit:           "986065b74c8e9e4012287fdd6b13021591ce00c3",
-			beforeCommitExclusive:  "13bd8316267d6a0be44f3f87fe49e807e7bc24b4",
-			expectedChangedFolders: []string{"app3", "app3/data", "app3/data/level2", "app1/data"},
-		},
-		"radixconfig-app3-level2.yaml was added to the subfolder of the folder app3": {
-			targetCommit:           "e89ac3d3ba66498cf6165e119d29a86b6b8183ab",
-			beforeCommitExclusive:  "986065b74c8e9e4012287fdd6b13021591ce00c3",
-			expectedChangedFolders: []string{"app3/data/level2"},
-		},
-		"radixconfig-app3-level2.yaml was added to the subfolder of the folder app3, it is current config": {
-			targetCommit:           "e89ac3d3ba66498cf6165e119d29a86b6b8183ab",
-			beforeCommitExclusive:  "986065b74c8e9e4012287fdd6b13021591ce00c3",
-			expectedChangedFolders: []string{"app3/data/level2"},
-		},
-		"radixconfig-app3-level2.yaml was added to the subfolder of the folder app3, it is current config, but not this branch": {
-			targetCommit:           "e89ac3d3ba66498cf6165e119d29a86b6b8183ab",
-			beforeCommitExclusive:  "986065b74c8e9e4012287fdd6b13021591ce00c3",
-			expectedChangedFolders: []string{"app3/data/level2"},
-		},
-		"Files were changed in subfolders app1 and app3, with config radixconfig.yaml": {
-			targetCommit:           "2127927fa21ae471baefbadd3f05b60a4bf38b5f",
-			beforeCommitExclusive:  "e89ac3d3ba66498cf6165e119d29a86b6b8183ab",
-			expectedChangedFolders: []string{"app1/data", "app3/data/level2"},
-		},
-		"Files were changed in subfolders app1 and app3, with config app3/data/level2/radixconfig-app3-level2.yaml": {
-			targetCommit:           "2127927fa21ae471baefbadd3f05b60a4bf38b5f",
-			beforeCommitExclusive:  "e89ac3d3ba66498cf6165e119d29a86b6b8183ab",
-			expectedChangedFolders: []string{"app1/data", "app3/data/level2"},
-		},
-		"Empty commit, made with 'git commit -m 'empty' --allow-empty": {
-			targetCommit:           "95b2136dd7f8bd8ec52ef46897d55ba321da3abe",
-			beforeCommitExclusive:  "cdbd98a036ae5a96f5ad97d2fb225aa5e550f4c1",
-			expectedChangedFolders: []string{},
-		},
-		"Changes of in-root file, used in the component with root source": {
-			targetCommit:           "8b5f81bd11c1670a6894cd14abcb1c9d2cde6e7e",
-			beforeCommitExclusive:  "d897c65f4322f4dc71b33ff2fd6b365f815a2026",
-			expectedChangedFolders: []string{"."},
-		},
-		"Changes of subfolder, used in the component with root source": {
-			targetCommit:           "192e173dc968368a5dc9f42abbcbd9dab6484194",
-			beforeCommitExclusive:  "8b5f81bd11c1670a6894cd14abcb1c9d2cde6e7e",
-			expectedChangedFolders: []string{"app3/data/level2"},
-		},
-		"Change of binary files is tracked correctly": {
-			targetCommit:           "efa70bd57f3a965aa13429ca856f54feaf3d2645",
-			beforeCommitExclusive:  "4a7624fd54c19adbf72bba72b0ae16829680db34",
-			expectedChangedFolders: []string{"app1/data/level2"},
-		},
-		"Change of LFS binary files is tracked correctly": {
-			targetCommit:           "cd65c2fcab588953c72f0af5350282c282051286",
-			beforeCommitExclusive:  "dd81c903e402b4120cbf2431393ced321b243b45",
-			expectedChangedFolders: []string{"app1/data/level2"},
+		"invalid before commit": {
+			targetCommit:  "fe148016c5fcd68e3a3b42d32f1c02586072defc",
+			beforeCommit:  "invalid-value",
+			expectedError: git.ErrCommitNotFound,
 		},
 	}
 
@@ -275,13 +188,13 @@ func Test_DiffCommits(t *testing.T) {
 			t.Run(fmt.Sprintf("%s (zip: %s, folder: %s)", scenarioName, zipFile, folder), func(t *testing.T) {
 				repo, err := git.Open(gitWorkspacePath)
 				require.NoError(t, err)
-				diffs, err := repo.DiffCommits(scenario.beforeCommitExclusive, scenario.targetCommit)
+				diffs, err := repo.DiffCommits(scenario.beforeCommit, scenario.targetCommit)
+				assert.ErrorIs(t, err, scenario.expectedError)
+
 				if scenario.expectedError == nil {
-					assert.NoError(t, err)
-					assert.ElementsMatch(t, scenario.expectedChangedFolders, diffs.Dirs())
-				} else {
-					assert.ErrorIs(t, err, scenario.expectedError)
+					assert.ElementsMatch(t, scenario.expectedDiffs, diffs)
 				}
+
 			})
 		}
 	}
@@ -290,58 +203,116 @@ func Test_DiffCommits(t *testing.T) {
 func Test_Checkout(t *testing.T) {
 	gitWorkspacePath := setupGitTest("test-data-git-commits.zip", "test-data-git-commits")
 	defer tearDownGitTest()
-
-	var (
-		zipFile     string = path.Join(gitWorkspacePath, "/app1/data/level2/test-data-git-commits-blobless.zip")
-		dockerFile  string = path.Join(gitWorkspacePath, "app1/Dockerfile")
-		featureFile string = path.Join(gitWorkspacePath, "feature.MD")
-	)
-
 	repo, err := git.Open(gitWorkspacePath)
 	require.NoError(t, err)
 
-	err = repo.Checkout("0b9ee1f93639fff492c05b8d5e662301f508debe")
-	assert.NoError(t, err)
-	_, err = os.Stat(zipFile)
-	assert.ErrorIs(t, err, os.ErrNotExist)
-	_, err = os.Stat(featureFile)
-	assert.ErrorIs(t, err, os.ErrNotExist)
-	fstat, err := os.Stat(dockerFile)
-	require.NoError(t, err)
-	assert.Equal(t, int64(84), fstat.Size())
+	const (
+		zipFile       = "/app1/data/level2/test-data-git-commits-blobless.zip"
+		gitIgnoreFile = ".gitignore"
+		featureFile   = "feature.MD"
+	)
 
-	err = repo.Checkout("dev")
-	assert.NoError(t, err)
-	_, err = os.Stat(zipFile)
-	assert.ErrorIs(t, err, os.ErrNotExist)
-	_, err = os.Stat(featureFile)
-	assert.ErrorIs(t, err, os.ErrNotExist)
-	fstat, err = os.Stat(dockerFile)
-	require.NoError(t, err)
-	assert.Equal(t, int64(88), fstat.Size())
+	type fileCheck struct {
+		path  string
+		exist bool
+		size  int64
+	}
 
-	err = repo.Checkout("main")
-	assert.NoError(t, err)
-	_, err = os.Stat(zipFile)
-	assert.NoError(t, err)
-	_, err = os.Stat(featureFile)
-	assert.ErrorIs(t, err, os.ErrNotExist)
-	fstat, err = os.Stat(dockerFile)
-	require.NoError(t, err)
-	assert.Equal(t, int64(88), fstat.Size())
+	tests := map[string]struct {
+		checkoutRef   string
+		fileChecks    []fileCheck
+		expectedError error
+	}{
+		"commit": {
+			checkoutRef: "0b9ee1f93639fff492c05b8d5e662301f508debe",
+			fileChecks: []fileCheck{
+				{path: zipFile, exist: false},
+				{path: gitIgnoreFile, exist: true, size: 5},
+				{path: featureFile, exist: false},
+			},
+		},
+		"dev branch": {
+			checkoutRef: "dev",
+			fileChecks: []fileCheck{
+				{path: zipFile, exist: false},
+				{path: gitIgnoreFile, exist: true, size: 21},
+				{path: featureFile, exist: false},
+			},
+		},
+		"main branch": {
+			checkoutRef: "main",
+			fileChecks: []fileCheck{
+				{path: zipFile, exist: true, size: 131}, // Due to lfs
+				{path: gitIgnoreFile, exist: true, size: 41},
+				{path: featureFile, exist: false},
+			},
+		},
+		"feature branch": {
+			checkoutRef: "feature",
+			fileChecks: []fileCheck{
+				{path: zipFile, exist: true, size: 131}, // Due to lfs
+				{path: gitIgnoreFile, exist: true, size: 41},
+				{path: featureFile, exist: true, size: 27},
+			},
+		},
+		"non existing ref": {
+			checkoutRef:   "non-existing",
+			expectedError: git.ErrReferenceNotFound,
+		},
+	}
 
-	err = repo.Checkout("feature")
-	assert.NoError(t, err)
-	_, err = os.Stat(zipFile)
-	assert.NoError(t, err)
-	_, err = os.Stat(featureFile)
-	assert.NoError(t, err)
-	fstat, err = os.Stat(dockerFile)
-	require.NoError(t, err)
-	assert.Equal(t, int64(88), fstat.Size())
+	for testName, testSpec := range tests {
+		t.Run(testName, func(t *testing.T) {
+			err := repo.Checkout(testSpec.checkoutRef)
+			assert.ErrorIs(t, err, testSpec.expectedError)
+			if testSpec.expectedError == nil {
+				for _, fc := range testSpec.fileChecks {
+					t.Run(fc.path, func(t *testing.T) {
+						fileStat, err := os.Stat(path.Join(gitWorkspacePath, fc.path))
+						if fc.exist {
+							require.NoError(t, err)
+							assert.Equal(t, fc.size, fileStat.Size())
+						} else {
+							assert.ErrorIs(t, err, os.ErrNotExist)
+						}
+					})
+				}
+			}
+		})
+	}
 
-	err = repo.Checkout("non-existing")
-	assert.Error(t, err)
+	// err = repo.Checkout("dev")
+	// assert.NoError(t, err)
+	// _, err = os.Stat(zipFile)
+	// assert.ErrorIs(t, err, os.ErrNotExist)
+	// _, err = os.Stat(featureFile)
+	// assert.ErrorIs(t, err, os.ErrNotExist)
+	// fstat, err = os.Stat(gitIgnoreFile)
+	// require.NoError(t, err)
+	// assert.Equal(t, int64(88), fstat.Size())
+
+	// err = repo.Checkout("main")
+	// assert.NoError(t, err)
+	// _, err = os.Stat(zipFile)
+	// assert.NoError(t, err)
+	// _, err = os.Stat(featureFile)
+	// assert.ErrorIs(t, err, os.ErrNotExist)
+	// fstat, err = os.Stat(gitIgnoreFile)
+	// require.NoError(t, err)
+	// assert.Equal(t, int64(88), fstat.Size())
+
+	// err = repo.Checkout("feature")
+	// assert.NoError(t, err)
+	// _, err = os.Stat(zipFile)
+	// assert.NoError(t, err)
+	// _, err = os.Stat(featureFile)
+	// assert.NoError(t, err)
+	// fstat, err = os.Stat(gitIgnoreFile)
+	// require.NoError(t, err)
+	// assert.Equal(t, int64(88), fstat.Size())
+
+	// err = repo.Checkout("non-existing")
+	// assert.Error(t, err)
 }
 
 func Test_ResolveCommitForReference(t *testing.T) {
