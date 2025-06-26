2065cf2fd172d428cb41c2432eed40a31ebdae5d
Richard87 <richard.hagen@gmail.com>
2025-06-26 17:08:09 +0200

    wip: wip

--- pkg/apis/radix/v1/radixregistrationtypes.go
+++ pkg/apis/radix/v1/radixregistrationtypes.go
@@ -30,23 +30,74 @@ type RadixRegistrationStatus struct {
 
 // RadixRegistrationSpec is the spec for an application
 type RadixRegistrationSpec struct {
-	AppID               ULID     `json:"appID,omitempty"`
-	CloneURL            string   `json:"cloneURL"`
-	SharedSecret        string   `json:"sharedSecret,omitempty"`
-	DeployKey           string   `json:"deployKey,omitempty"`
-	DeployKeyPublic     string   `json:"deployKeyPublic,omitempty"`
-	AdGroups            []string `json:"adGroups,omitempty"`
-	AdUsers             []string `json:"adUsers,omitempty"`
-	ReaderAdGroups      []string `json:"readerAdGroups,omitempty"`
-	ReaderAdUsers       []string `json:"readerAdUsers,omitempty"`
-	Creator             string   `json:"creator,omitempty"`
-	Owner               string   `json:"owner,omitempty"`
-	WBS                 string   `json:"wbs,omitempty"`
-	ConfigBranch        string   `json:"configBranch"`
-	RadixConfigFullName string   `json:"radixConfigFullName,omitempty"`
+
+	// AppID is the unique identifier for the Radix application. Not to be confused by Configuration Item.
+	// +optional
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="Value is immutable"
+	AppID ULID `json:"appID"` // omitempty was configured, but aparently does not work with nested structs
+
+	// CloneURL is the URL of the GitHub repository where the Radix configuration file is located.
+	// +required
+	// +kubebuilder:validation:Pattern=`^(git@github.com:)([\w-]+)/([\w-]+)(.git)$`
+	CloneURL string `json:"cloneURL"`
+
+	// deprecated: SharedSecret is the shared secret for the git repository.
+	// +optional
+	SharedSecret string `json:"sharedSecret,omitempty"`
+
+	// deprecated: DeployKey is the deploy key for the git repository.
+	// +optional
+	DeployKey string `json:"deployKey,omitempty"`
+
+	// deprecated: DeployKeyPublic is the public key for the deploy key.
+	// +optional
+	DeployKeyPublic string `json:"deployKeyPublic,omitempty"`
+
+	// +optional
+	// +kubebuilder:validation:items:Format=uuid
+	// +kubebuilder:validation:items:Pattern=`^([A-Za-z0-9]{8})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{12})$`
+	AdGroups []string `json:"adGroups,omitempty"`
+
+	// +optional
+	// +kubebuilder:validation:items:Format=uuid
+	// +kubebuilder:validation:items:Pattern=`^([A-Za-z0-9]{8})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{12})$`
+	AdUsers []string `json:"adUsers,omitempty"`
+
+	// +optional
+	// +kubebuilder:validation:items:Format=uuid
+	// +kubebuilder:validation:items:Pattern=`^([A-Za-z0-9]{8})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{12})$`
+	ReaderAdGroups []string `json:"readerAdGroups,omitempty"`
+
+	// +optional
+	// +kubebuilder:validation:items:Format=uuid
+	// +kubebuilder:validation:items:Pattern=`^([A-Za-z0-9]{8})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{12})$`
+	ReaderAdUsers []string `json:"readerAdUsers,omitempty"`
+
+	// +optional
+	Creator string `json:"creator,omitempty"`
+
+	// +optional
+	Owner string `json:"owner,omitempty"`
+
+	// WBS: Deprecated: WBS was previously used to identify the application in a WBS (Work Breakdown Structure).
+	// deprecated: use ConfigurationItem instead
+	WBS string `json:"wbs,omitempty"`
+
+	// ConfigBranch is the branch in the git repository where the Radix configuration file is located. See https://git-scm.com/docs/git-check-ref-format#_description for more details.
+	// +required
+	// +kubebuilder:validation:XValidation:rule=`!(  self.endsWith('.lock') ||  self.contains('..') ||  self.matches('.*[\\x00-\\x1F\\x7F ~^:\\?\\*\\[].*') ||  self.contains('@{') ||  self.contains('\\') ||  self.startsWith('/') ||  self.endsWith('/') ||  self.contains('//') ||  self.endsWith('.') ||  self == '@' ||  self == '' || self.split('/').maxLength(10) || self.split('/').exists(comp, comp.startsWith('.') || comp.endsWith('.lock')))`
+	ConfigBranch string `json:"configBranch"`
+
+	// RadixConfigFullName is the full name of the Radix configuration file in the git repository.
+	// +optional
+	// +kubebuilder:validation:Pattern=`^(\/*[a-zA-Z0-9_\.\-]+)+((\.yaml)|(\.yml))$`
+	RadixConfigFullName string `json:"radixConfigFullName,omitempty"`
+
 	// ConfigurationItem is and identifier for an entity in a configuration management solution such as a CMDB.
+	// TODO: Should be ServiceNow AppID, is currently a mix of name, and CMDB SysID.
 	// ITIL defines a CI as any component that needs to be managed in order to deliver an IT Service
 	// Ref: https://en.wikipedia.org/wiki/Configuration_item
+	// +kubebuilder:validation:MaxLength=100
 	ConfigurationItem string `json:"configurationItem,omitempty"`
 }
 
--- pkg/apis/radixvalidators/validate_rr.go
+++ pkg/apis/radixvalidators/validate_rr.go
@@ -3,7 +3,6 @@ package radixvalidators
 import (
 	"context"
 	stderrors "errors"
-	"fmt"
 	"regexp"
 	"strings"
 
@@ -24,12 +23,7 @@ var (
 	validUuidRegex                = regexp.MustCompile("^([A-Za-z0-9]{8})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{12})$")
 
 	requiredRadixRegistrationValidators = []RadixRegistrationValidator{
-		validateRadixRegistrationAppName,
-		validateRadixRegistrationGitSSHUrl,
-		validateRadixRegistrationSSHKey,
-		validateRadixRegistrationAdConfig,
 		validateRadixRegistrationConfigBranch,
-		validateRadixRegistrationConfigurationItem,
 	}
 )
 
@@ -57,7 +51,7 @@ func RequireAdGroups(rr *v1.RadixRegistration) error {
 // CanRadixRegistrationBeInserted Validates RR
 func CanRadixRegistrationBeInserted(ctx context.Context, client radixclient.Interface, radixRegistration *v1.RadixRegistration, additionalValidators ...RadixRegistrationValidator) error {
 	// cannot be used from admission control - returns the same radix reg that we try to validate
-	validators := append(requiredRadixRegistrationValidators, validateRadixRegistrationAppNameAvailableFactory(ctx, client))
+	validators := append(requiredRadixRegistrationValidators)
 	validators = append(validators, additionalValidators...)
 	return validateRadixRegistration(radixRegistration, validators...)
 }
@@ -88,38 +82,6 @@ func GetRadixRegistrationBeUpdatedWarnings(ctx context.Context, client radixclie
 	return appendNoDuplicateGitRepoWarning(ctx, client, radixRegistration.Name, radixRegistration.Spec.CloneURL)
 }
 
-func validateRadixRegistrationAppNameAvailableFactory(ctx context.Context, client radixclient.Interface) RadixRegistrationValidator {
-	return func(radixRegistration *v1.RadixRegistration) error {
-		return validateDoesNameAlreadyExist(ctx, client, radixRegistration.Name)
-	}
-}
-
-func validateDoesNameAlreadyExist(ctx context.Context, client radixclient.Interface, appName string) error {
-	rr, _ := client.RadixV1().RadixRegistrations().Get(ctx, appName, metav1.GetOptions{})
-	if rr != nil && rr.Name != "" {
-		return fmt.Errorf("app name must be unique in cluster - %s already exist", appName)
-	}
-	return nil
-}
-
-func validateRadixRegistrationAppName(rr *v1.RadixRegistration) error {
-	return validateAppName(rr.Name)
-}
-
-func validateAppName(appName string) error {
-	return validateRequiredResourceName("app name", appName, 253)
-}
-
-func validateRadixRegistrationConfigurationItem(rr *v1.RadixRegistration) error {
-	return validateConfigurationItem(rr.Spec.ConfigurationItem)
-}
-
-func validateConfigurationItem(value string) error {
-	if len(value) > 100 {
-		return InvalidStringValueMaxLengthErrorWithMessage("configuration item", value, 100)
-	}
-	return nil
-}
 
 func validateRequiredResourceName(resourceName, value string, maxLength int) error {
 	if len(value) > maxLength {
@@ -140,46 +102,6 @@ func validateRequiredResourceName(resourceName, value string, maxLength int) err
 	return nil
 }
 
-func validateRadixRegistrationAdConfig(rr *v1.RadixRegistration) error {
-	errs := []error{}
-	for _, group := range rr.Spec.AdGroups {
-		if !validUuidRegex.MatchString(group) {
-			errs = append(errs, fmt.Errorf("%w: %s", ErrInvalidEntraUuid, group))
-		}
-	}
-	for _, group := range rr.Spec.AdUsers {
-		if !validUuidRegex.MatchString(group) {
-			errs = append(errs, fmt.Errorf("%w: %s", ErrInvalidEntraUuid, group))
-		}
-	}
-
-	err := stderrors.Join(errs...)
-	if err != nil {
-		return fmt.Errorf("radix registration Entra validation error: %w", err)
-	}
-
-	return nil
-}
-
-func validateRadixRegistrationGitSSHUrl(rr *v1.RadixRegistration) error {
-	return validateGitSSHUrl(rr.Spec.CloneURL)
-}
-
-func validateGitSSHUrl(sshURL string) error {
-	re := regexp.MustCompile(`^(git@github.com:)([\w-]+)/([\w-]+)(.git)$`)
-
-	if sshURL == "" {
-		return fmt.Errorf("ssh url is required")
-	}
-
-	isValid := re.MatchString(sshURL)
-
-	if isValid {
-		return nil
-	}
-	return fmt.Errorf("ssh url not valid %s. Must match regex %s", sshURL, re.String())
-}
-
 func appendNoDuplicateGitRepoWarning(ctx context.Context, client radixclient.Interface, appName, sshURL string) ([]string, error) {
 	if sshURL == "" {
 		return nil, nil
@@ -196,15 +118,6 @@ func appendNoDuplicateGitRepoWarning(ctx context.Context, client radixclient.Int
 		}
 	}
 	return nil, nil
-}
-
-func validateRadixRegistrationSSHKey(rr *v1.RadixRegistration) error {
-	return validateSSHKey(rr.Spec.DeployKey)
-}
-
-func validateSSHKey(_ string) error {
-	// todo - how can this be validated..e.g. checked that the key isn't protected by a password
-	return nil
 }
 
 func validateDoesRRExist(ctx context.Context, client radixclient.Interface, appName string) error {
