6839e61cea714bb0f89adf5d79e72be817a05f7e
Richard87 <richard.hagen@gmail.com>
2025-07-03 13:21:50 +0200

    fix: CERTS_DIR is required and must exist

--- webhook/internal/config/config.go
+++ webhook/internal/config/config.go
@@ -11,20 +11,20 @@ import (
 var Version = "dev (unknown)"
 
 type Config struct {
-	Port           int    `envconfig:"PORT" default:"9443" desc:"The address the health endpoint binds to"`
-	MetricsPort    int    `envconfig:"METRICS_PORT" default:"9000"  desc:"The address the metric endpoint binds to."`
-	HealthPort     int    `envconfig:"HEALTH_PORT" default:"9440" desc:"The address the health endpoint binds to"`
-	LogLevel       string `envconfig:"LOG_LEVEL" default:"info"`
-	LogPrettyPrint bool   `envconfig:"LOG_PRETTY" default:"false"`
+	CertsDir string `envconfig:"CERTS_DIR" required:"true" desc:"Directory where the webhook TLS certificate is stored"`
 
 	RequireAdGroups          bool `envconfig:"REQUIRE_AD_GROUPS" default:"false" desc:"Require AD groups for authentication"`
 	RequireConfigurationItem bool `envconfig:"REQUIRE_CONFIGURATION_ITEM" default:"false" desc:"Require configuration item for authentication"`
 
-	SecretName      string `envconfig:"SECRET_NAME" default:"radix-webhook-certs" desc:"Name of the secret where the webhook TLS certificate is stored"`
-	SecretNamespace string `envconfig:"SECRET_NAMESPACE" default:"default" desc:"Namespace of the secret where the webhook TLS certificate is stored"`
+	LogLevel       string `envconfig:"LOG_LEVEL" default:"info" desc:"Log level for the application. Possible values: debug, info, warn, error, fatal"`
+	LogPrettyPrint bool   `envconfig:"LOG_PRETTY" default:"false" desc:"Enable pretty print for logs. If set to true, the logs will be formatted in a human-readable way."`
+	Port           int    `envconfig:"PORT" default:"9443" desc:"The address the health endpoint binds to"`
+	MetricsPort    int    `envconfig:"METRICS_PORT" default:"9000"  desc:"The address the metric endpoint binds to."`
+	HealthPort     int    `envconfig:"HEALTH_PORT" default:"9440" desc:"The address the health endpoint binds to"`
 
+	SecretName               string   `envconfig:"SECRET_NAME" default:"radix-webhook-certs" desc:"Name of the secret where the webhook TLS certificate is stored"`
+	SecretNamespace          string   `envconfig:"SECRET_NAMESPACE" default:"default" desc:"Namespace of the secret where the webhook TLS certificate is stored"`
 	DisableCertRotation      bool     `envconfig:"DISABLE_CERT_ROTATION" default:"false" desc:"Disable automatic certificate rotation"`
-	CertsDir                 string   `envconfig:"CERTS_DIR" default:"/certs" desc:"Directory where the webhook TLS certificate is stored"`
 	CaName                   string   `envconfig:"CA_NAME" default:"radix-webhook-ca" desc:"Name of the CA secret"`
 	CaOrganization           string   `envconfig:"CA_ORGANIZATION" default:"Radix Webhook CA" desc:"Organization of the CA"`
 	DnsName                  string   `envconfig:"DNS_NAME" default:"radix-webhook.default.svc" desc:"DNS name of the webhook service"`
