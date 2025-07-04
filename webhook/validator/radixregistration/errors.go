8fa3be99a0ebbc3b13a7ccbebec32bf3f80f653f
Richard Hagen <richard.hagen@gmail.com>
2025-07-04 13:03:04 +0200

    Update webhook/validator/radixregistration/errors.go
    Co-authored-by: Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>

--- webhook/validator/radixregistration/errors.go
+++ webhook/validator/radixregistration/errors.go
@@ -8,5 +8,5 @@ var (
 	ErrAdGroupIsRequired                    = errors.New("ad group is required")
 	ErrConfigurationItemIsRequired          = errors.New("configuration item is required")
 	ErrAppIdMustBeUnique                    = errors.New("app id must be unique")
-	WarningAdGroupsShouldHaveAtleastOneItem = "warning: adGroups should have atleast one item"
+	WarningAdGroupsShouldHaveAtleastOneItem = "warning: adGroups should have at least one item"
 )
