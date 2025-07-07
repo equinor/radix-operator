3f53895d3ce852d762384c6799ef14d7c7c2a484
Richard87 <richard.hagen@gmail.com>
2025-07-07 12:15:22 +0200

    fix: simplify type handling in InstantiateGenericStruct function

--- pkg/apis/utils/generic/generic.go
+++ pkg/apis/utils/generic/generic.go
@@ -14,14 +14,10 @@ func InstantiateGenericStruct[TObj any]() TObj {
 		// If TObj is a pointer type, we need to get the element type
 		valueType = valueType.Elem()
 		return reflect.New(valueType).Interface().(TObj)
-	case reflect.Struct:
-		// If TObj is a struct type, we can return a zero value of that type
-		return reflect.Zero(valueType).Interface().(TObj)
-	case reflect.String, reflect.Int, reflect.Float64, reflect.Bool:
+	case reflect.Struct, reflect.String, reflect.Int, reflect.Float64, reflect.Bool:
 		// If TObj is a basic type, return its zero value
 		return reflect.Zero(valueType).Interface().(TObj)
 	default:
-		// If TObj is neither a struct nor a pointer to struct, panic
-		panic("TObj must be a struct or pointer to struct type")
+		panic("InstantiateGenericStruct: unsupported type " + valueType.String())
 	}
 }
