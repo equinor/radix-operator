c34a436a036eb531e493ee2e9d54f16aec600518
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-10-02 12:41:44 +0200

    add more tests

--- velero-plugin/internal/plugin/restore/restore_radix_application_test.go
+++ velero-plugin/internal/plugin/restore/restore_radix_application_test.go
@@ -19,39 +19,39 @@ import (
 	clienttesting "k8s.io/client-go/testing"
 )
 
-type restoreRadixAlertPluginTest struct {
+type restoreRadixApplicationPluginTest struct {
 	suite.Suite
 	kubeClient  *fake.Clientset
 	radixClient *fakeradix.Clientset
 	kubeUtil    *kube.Kube
 }
 
-func TestRestoreRadixAlertPlugin(t *testing.T) {
-	suite.Run(t, new(restoreRadixAlertPluginTest))
+func TestRestoreRadixApplicationPlugin(t *testing.T) {
+	suite.Run(t, new(restoreRadixApplicationPluginTest))
 }
 
-func (s *restoreRadixAlertPluginTest) SetupTest() {
+func (s *restoreRadixApplicationPluginTest) SetupTest() {
 	s.kubeClient = fake.NewSimpleClientset()
 	s.radixClient = fakeradix.NewSimpleClientset()
 	s.kubeUtil, _ = kube.New(s.kubeClient, s.radixClient, nil, nil)
 }
 
-func (s *restoreRadixAlertPluginTest) Test_AppliesTo() {
-	expected := velero.ResourceSelector{IncludedResources: []string{"radixalerts.radix.equinor.com"}}
-	plugin := restore.RestoreAlertPlugin{}
+func (s *restoreRadixApplicationPluginTest) Test_AppliesTo() {
+	expected := velero.ResourceSelector{IncludedResources: []string{"radixapplications.radix.equinor.com"}}
+	plugin := restore.RestoreRadixApplicationPlugin{}
 	actual, err := plugin.AppliesTo()
 	s.NoError(err)
 	s.Equal(expected, actual)
 }
 
-func (s *restoreRadixAlertPluginTest) Test_Execute_RadixClientError() {
+func (s *restoreRadixApplicationPluginTest) Test_Execute_RadixClientError() {
 	getError := errors.New("any error")
 	s.radixClient.PrependReactor("get", "radixregistrations", func(action clienttesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, nil, getError
 
 	})
 
-	source := radixv1.RadixAlert{}
+	source := radixv1.RadixApplication{}
 	sourceUnstructured, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&source)
 	s.Require().NoError(err)
 	input := &velero.RestoreItemActionExecuteInput{
@@ -59,7 +59,7 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixClientError() {
 		ItemFromBackup: &unsturctured.Unstructured{Object: sourceUnstructured},
 	}
 
-	plugin := restore.RestoreAlertPlugin{
+	plugin := restore.RestoreRadixApplicationPlugin{
 		Log:  logrus.New(),
 		Kube: s.kubeUtil,
 	}
@@ -68,12 +68,10 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixClientError() {
 	s.ErrorIs(err, getError)
 }
 
-func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationMissing() {
-	source := radixv1.RadixAlert{
+func (s *restoreRadixApplicationPluginTest) Test_Execute_RadixRegistrationMissing() {
+	source := radixv1.RadixApplication{
 		ObjectMeta: v1.ObjectMeta{
-			Labels: map[string]string{
-				kube.RadixAppLabel: "any-app",
-			},
+			Name: "any-app",
 		},
 	}
 	sourceUnstructured, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&source)
@@ -83,7 +81,7 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationMissing() {
 		ItemFromBackup: &unsturctured.Unstructured{Object: sourceUnstructured},
 	}
 
-	plugin := restore.RestoreAlertPlugin{
+	plugin := restore.RestoreRadixApplicationPlugin{
 		Log:  logrus.New(),
 		Kube: s.kubeUtil,
 	}
@@ -93,7 +91,7 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationMissing() {
 	s.True(output.SkipRestore)
 }
 
-func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationExist() {
+func (s *restoreRadixApplicationPluginTest) Test_Execute_RadixRegistrationExist() {
 	const appName = "any-app"
 	rr := &radixv1.RadixRegistration{
 		ObjectMeta: v1.ObjectMeta{
@@ -102,15 +100,13 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationExist() {
 	}
 	_, err := s.radixClient.RadixV1().RadixRegistrations().Create(context.Background(), rr, v1.CreateOptions{})
 	s.Require().NoError(err)
-	source := radixv1.RadixAlert{
+	source := radixv1.RadixApplication{
 		ObjectMeta: v1.ObjectMeta{
-			Labels: map[string]string{
-				kube.RadixAppLabel: appName,
-			},
+			Name: appName,
 		},
-		Spec: radixv1.RadixAlertSpec{
-			Alerts: []radixv1.Alert{
-				{Alert: "foo", Receiver: "bar"},
+		Spec: radixv1.RadixApplicationSpec{
+			Environments: []radixv1.Environment{
+				{Name: "qa"},
 			},
 		},
 	}
@@ -121,7 +117,7 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationExist() {
 		ItemFromBackup: &unsturctured.Unstructured{Object: sourceUnstructured},
 	}
 
-	plugin := restore.RestoreAlertPlugin{
+	plugin := restore.RestoreRadixApplicationPlugin{
 		Log:  logrus.New(),
 		Kube: s.kubeUtil,
 	}
@@ -129,7 +125,7 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationExist() {
 	output, err := plugin.Execute(input)
 	s.Require().NoError(err)
 	s.False(output.SkipRestore)
-	var outputSource radixv1.RadixAlert
+	var outputSource radixv1.RadixApplication
 	err = runtime.DefaultUnstructuredConverter.FromUnstructured(output.UpdatedItem.UnstructuredContent(), &outputSource)
 	s.Require().NoError(err)
 	s.Equal(source, outputSource)
--- velero-plugin/internal/plugin/restore/restore_secret_test.go
+++ velero-plugin/internal/plugin/restore/restore_secret_test.go
@@ -12,46 +12,43 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/suite"
 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	unsturctured "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
 	clienttesting "k8s.io/client-go/testing"
 )
 
-type restoreRadixAlertPluginTest struct {
+type restoreSecretPluginTest struct {
 	suite.Suite
 	kubeClient  *fake.Clientset
 	radixClient *fakeradix.Clientset
 	kubeUtil    *kube.Kube
 }
 
-func TestRestoreRadixAlertPlugin(t *testing.T) {
-	suite.Run(t, new(restoreRadixAlertPluginTest))
+func TestRestoreSecretPlugin(t *testing.T) {
+	suite.Run(t, new(restoreSecretPluginTest))
 }
 
-func (s *restoreRadixAlertPluginTest) SetupTest() {
+func (s *restoreSecretPluginTest) SetupTest() {
 	s.kubeClient = fake.NewSimpleClientset()
 	s.radixClient = fakeradix.NewSimpleClientset()
 	s.kubeUtil, _ = kube.New(s.kubeClient, s.radixClient, nil, nil)
 }
 
-func (s *restoreRadixAlertPluginTest) Test_AppliesTo() {
-	expected := velero.ResourceSelector{IncludedResources: []string{"radixalerts.radix.equinor.com"}}
-	plugin := restore.RestoreAlertPlugin{}
+func (s *restoreSecretPluginTest) Test_AppliesTo() {
+	expected := velero.ResourceSelector{IncludedResources: []string{"secrets"}}
+	plugin := restore.RestoreSecretPlugin{}
 	actual, err := plugin.AppliesTo()
 	s.NoError(err)
 	s.Equal(expected, actual)
 }
 
-func (s *restoreRadixAlertPluginTest) Test_Execute_RadixClientError() {
-	getError := errors.New("any error")
-	s.radixClient.PrependReactor("get", "radixregistrations", func(action clienttesting.Action) (handled bool, ret runtime.Object, err error) {
-		return true, nil, getError
-
-	})
-
-	source := radixv1.RadixAlert{}
+func (s *restoreSecretPluginTest) Test_Execute_RadixAppLabelNotSet() {
+	source := corev1.Secret{
+		Data: map[string][]byte{"foo": []byte("bar")},
+	}
 	sourceUnstructured, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&source)
 	s.Require().NoError(err)
 	input := &velero.RestoreItemActionExecuteInput{
@@ -59,18 +56,29 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixClientError() {
 		ItemFromBackup: &unsturctured.Unstructured{Object: sourceUnstructured},
 	}
 
-	plugin := restore.RestoreAlertPlugin{
+	plugin := restore.RestoreSecretPlugin{
 		Log:  logrus.New(),
 		Kube: s.kubeUtil,
 	}
 
-	_, err = plugin.Execute(input)
-	s.ErrorIs(err, getError)
+	output, err := plugin.Execute(input)
+	s.Require().NoError(err)
+	s.False(output.SkipRestore)
+	var outputSource corev1.Secret
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(output.UpdatedItem.UnstructuredContent(), &outputSource)
+	s.Require().NoError(err)
+	s.Equal(source, outputSource)
 }
 
-func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationMissing() {
-	source := radixv1.RadixAlert{
-		ObjectMeta: v1.ObjectMeta{
+func (s *restoreSecretPluginTest) Test_Execute_RadixClientError() {
+	getError := errors.New("any error")
+	s.radixClient.PrependReactor("get", "radixregistrations", func(action clienttesting.Action) (handled bool, ret runtime.Object, err error) {
+		return true, nil, getError
+
+	})
+
+	source := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
 			Labels: map[string]string{
 				kube.RadixAppLabel: "any-app",
 			},
@@ -83,34 +91,20 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationMissing() {
 		ItemFromBackup: &unsturctured.Unstructured{Object: sourceUnstructured},
 	}
 
-	plugin := restore.RestoreAlertPlugin{
+	plugin := restore.RestoreSecretPlugin{
 		Log:  logrus.New(),
 		Kube: s.kubeUtil,
 	}
 
-	output, err := plugin.Execute(input)
-	s.Require().NoError(err)
-	s.True(output.SkipRestore)
+	_, err = plugin.Execute(input)
+	s.ErrorIs(err, getError)
 }
 
-func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationExist() {
-	const appName = "any-app"
-	rr := &radixv1.RadixRegistration{
-		ObjectMeta: v1.ObjectMeta{
-			Name: appName,
-		},
-	}
-	_, err := s.radixClient.RadixV1().RadixRegistrations().Create(context.Background(), rr, v1.CreateOptions{})
-	s.Require().NoError(err)
-	source := radixv1.RadixAlert{
-		ObjectMeta: v1.ObjectMeta{
+func (s *restoreSecretPluginTest) Test_Execute_RadixRegistrationMissing() {
+	source := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
 			Labels: map[string]string{
-				kube.RadixAppLabel: appName,
-			},
-		},
-		Spec: radixv1.RadixAlertSpec{
-			Alerts: []radixv1.Alert{
-				{Alert: "foo", Receiver: "bar"},
+				kube.RadixAppLabel: "any-app",
 			},
 		},
 	}
@@ -121,7 +115,41 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationExist() {
 		ItemFromBackup: &unsturctured.Unstructured{Object: sourceUnstructured},
 	}
 
-	plugin := restore.RestoreAlertPlugin{
+	plugin := restore.RestoreSecretPlugin{
+		Log:  logrus.New(),
+		Kube: s.kubeUtil,
+	}
+
+	output, err := plugin.Execute(input)
+	s.Require().NoError(err)
+	s.True(output.SkipRestore)
+}
+
+func (s *restoreSecretPluginTest) Test_Execute_RadixRegistrationExist() {
+	const appName = "any-app"
+	rr := &radixv1.RadixRegistration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: appName,
+		},
+	}
+	_, err := s.radixClient.RadixV1().RadixRegistrations().Create(context.Background(), rr, metav1.CreateOptions{})
+	s.Require().NoError(err)
+	source := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{
+				kube.RadixAppLabel: appName,
+			},
+		},
+		Data: map[string][]byte{"foo": []byte("bar")},
+	}
+	sourceUnstructured, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&source)
+	s.Require().NoError(err)
+	input := &velero.RestoreItemActionExecuteInput{
+		Item:           &unsturctured.Unstructured{Object: sourceUnstructured},
+		ItemFromBackup: &unsturctured.Unstructured{Object: sourceUnstructured},
+	}
+
+	plugin := restore.RestoreSecretPlugin{
 		Log:  logrus.New(),
 		Kube: s.kubeUtil,
 	}
@@ -129,7 +157,7 @@ func (s *restoreRadixAlertPluginTest) Test_Execute_RadixRegistrationExist() {
 	output, err := plugin.Execute(input)
 	s.Require().NoError(err)
 	s.False(output.SkipRestore)
-	var outputSource radixv1.RadixAlert
+	var outputSource corev1.Secret
 	err = runtime.DefaultUnstructuredConverter.FromUnstructured(output.UpdatedItem.UnstructuredContent(), &outputSource)
 	s.Require().NoError(err)
 	s.Equal(source, outputSource)
