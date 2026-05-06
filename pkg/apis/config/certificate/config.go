91ebecd56570417be87458652c32f3b1d56cbeb6
Richard Hagen <richard.hagen@gmail.com>
2026-05-06 09:01:50 +0200

    refactor: remove deprecated ClusterIssuer references and update related configurations

--- pkg/apis/config/certificate/config.go
+++ pkg/apis/config/certificate/config.go
@@ -3,8 +3,6 @@ package certificate
 import "time"
 
 type AutomationConfig struct {
-	// Deprecated: ClusterIssuer is deprecated and will be removed in a future release. Use GatewayClusterIssuer instead.
-	ClusterIssuer        string
 	GatewayClusterIssuer string
 	Duration             time.Duration
 	RenewBefore          time.Duration
