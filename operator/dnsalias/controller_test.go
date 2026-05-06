f7292db713cacf422384f6b7d05bce161835ad4a
Richard Hagen <richard.hagen@gmail.com>
2026-05-06 09:05:19 +0200

    refactor: simplify RadixDNSAlias creation in test by removing unnecessary registration

--- operator/dnsalias/controller_test.go
+++ operator/dnsalias/controller_test.go
@@ -51,15 +51,11 @@ func (s *controllerTestSuite) Test_RadixDNSAliasEvents() {
 		componentName1 = "server1"
 		componentName2 = "server2"
 	)
-	_, err := s.RadixClient.RadixV1().RadixRegistrations().Create(context.Background(), &radixv1.RadixRegistration{
-		ObjectMeta: metav1.ObjectMeta{Name: appName1, Annotations: map[string]string{}},
-	}, metav1.CreateOptions{})
-	s.Require().NoError(err)
 	alias := internal.BuildRadixDNSAlias(appName1, componentName1, envName1, aliasName)
 
 	// Adding a RadixDNSAlias should trigger sync
 	s.Handler.EXPECT().Sync(gomock.Any(), "", aliasName).DoAndReturn(s.SyncedChannelCallback()).Times(1)
-	alias, err = s.RadixClient.RadixV1().RadixDNSAliases().Create(context.Background(), alias, metav1.CreateOptions{})
+	alias, err := s.RadixClient.RadixV1().RadixDNSAliases().Create(context.Background(), alias, metav1.CreateOptions{})
 	s.Require().NoError(err)
 	s.WaitForSynced("Sync should be called on add RadixDNSAlias")
 
