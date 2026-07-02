be67e44c4448654b1f59096573b1ddb69d89597a
Richard Hagen <richard.hagen@gmail.com>
2026-07-02 13:36:29 +0200

    fix: remove ACR secrets (#1815)

--- pkg/apis/defaults/commonsecrets.go
+++ pkg/apis/defaults/commonsecrets.go
@@ -1,9 +1,6 @@
 package defaults
 
 const (
-	// AzureACRServicePrincipleSecretName name of the secret containing ACR authentication information, consumed by az cli
-	AzureACRServicePrincipleSecretName = "radix-sp-acr-azure"
-
 	// AzureACRServicePrincipleBuildahSecretName name of the secret containing ACR authentication information, consumed by buildah
 	AzureACRServicePrincipleBuildahSecretName = "radix-sp-buildah-azure"
 
