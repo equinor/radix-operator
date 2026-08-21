e20235d2a61154c994b175a8caff97a59b4b12ae
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-08-21 09:53:02 +0200

    fix: correct import alias for middleware in server.go

--- job-scheduler/router/server.go
+++ job-scheduler/router/server.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/equinor/radix-operator/job-scheduler/api/v1/controllers"
-	commongin "github.com/equinor/radix-operator/job-scheduler/internal/middleware"
+	"github.com/equinor/radix-operator/job-scheduler/internal/middleware"
 	"github.com/equinor/radix-operator/job-scheduler/models"
 	"github.com/equinor/radix-operator/job-scheduler/swaggerui"
 	"github.com/gin-gonic/gin"
@@ -20,8 +20,8 @@ func NewServer(env *models.Env, controllers ...controllers.Controller) http.Hand
 	gin.SetMode(gin.ReleaseMode)
 	engine := gin.New()
 	engine.RemoveExtraSlash = true
-	engine.Use(commongin.SetZerologLogger(commongin.ZerologLoggerWithRequestId))
-	engine.Use(commongin.ZerologRequestLogger(), gin.Recovery())
+	engine.Use(middleware.SetZerologLogger(middleware.ZerologLoggerWithRequestId))
+	engine.Use(middleware.ZerologRequestLogger(), gin.Recovery())
 
 	if env.UseSwagger {
 		initializeSwaggerUI(engine)
