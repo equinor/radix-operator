ac03e70cefa7ddb8f818df5a1b43386a7d183d0f
herda1 <hera@equinor.com>
2026-08-31 09:37:18 +0200

    fix: remove buildFromBranch from deploymentSummary

--- api-server/api/models/environment_summary.go
+++ api-server/api/models/environment_summary.go
@@ -38,13 +38,8 @@ func BuildEnvironmentSummaryList(rr *radixv1.RadixRegistration, ra *radixv1.Radi
 
 	for _, re := range slice.FindAll(reList, predicate.IsOrphanEnvironment) {
 		deploymentSummary := getActiveDeploymentSummary(ra.GetName(), re.Spec.EnvName, rdList)
-		buildFromBranch := ""
-		if deploymentSummary != nil && deploymentSummary.GitRefType == "branch" {
-			buildFromBranch = deploymentSummary.GitRef
-		}
 		env := &environmentModels.EnvironmentSummary{
 			Name:             re.Spec.EnvName,
-			BranchMapping:    buildFromBranch,
 			ActiveDeployment: deploymentSummary,
 			Status:           getEnvironmentConfigurationStatus(&re).String(),
 		}
