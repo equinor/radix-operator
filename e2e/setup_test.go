0c30a85feabf4c2ad669b9a2b80cac1cb6f85680
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-09-07 11:26:08 +0200

    feat: add dnsZone configuration for e2e tests

--- e2e/setup_test.go
+++ e2e/setup_test.go
@@ -223,6 +223,7 @@ func TestMain(m *testing.M) {
 		"config.operator.containerRegistry":     "local-kind-repo",
 		"config.operator.appContainerRegistry":  "local-kind-repo",
 		"config.common.clusterName":             "weekly-e2e",
+		"config.common.dnsZone":                 "radix.example.com",
 		"rbac.createApp.groups[0]":              "123",
 		"image.pullPolicy":                      "IfNotPresent",
 		"radixPipelineRunner.image.pullPolicy":  "IfNotPresent",
