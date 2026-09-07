e306b9ba1732acf0eb934f92f778091be452e640
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-09-07 09:59:16 +0200

    feat: add PodSecurityStandard label generation and corresponding tests

--- pkg/apis/utils/labels/podsecuritystandard.go
+++ pkg/apis/utils/labels/podsecuritystandard.go
@@ -2,11 +2,12 @@ package labels
 
 import (
 	"github.com/equinor/radix-operator/pkg/apis/config2"
+	kubelabels "k8s.io/apimachinery/pkg/labels"
 )
 
 // PodSecurityStandardFromConfig builds pod security standard labels for a namespace.
-func PodSecurityStandardFromConfig(cfg config2.PodSecurityStandardPolicyConfig) map[string]string {
-	labels := make(map[string]string)
+func PodSecurityStandardFromConfig(cfg config2.PodSecurityStandardPolicyConfig) kubelabels.Set {
+	labels := make(kubelabels.Set)
 	if cfg.Enforce.Level != "" && cfg.Enforce.Version != "" {
 		labels["pod-security.kubernetes.io/enforce"] = cfg.Enforce.Level
 		labels["pod-security.kubernetes.io/enforce-version"] = cfg.Enforce.Version
