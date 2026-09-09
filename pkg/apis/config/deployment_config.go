2cbf354dbe6e6c87d4ac5b00bf4c24773d7d37b7
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-09-09 08:25:19 +0200

    fix formatting

--- pkg/apis/config/deployment_config.go
+++ pkg/apis/config/deployment_config.go
@@ -1,5 +1,5 @@
 package config
 
 type DeploymentSyncerConfig struct {
-	DeploymentHistoryLimit int   `envconfig:"RADIX_DEPLOYMENTS_PER_ENVIRONMENT_HISTORY_LIMIT" required:"true"`
+	DeploymentHistoryLimit int `envconfig:"RADIX_DEPLOYMENTS_PER_ENVIRONMENT_HISTORY_LIMIT" required:"true"`
 }
